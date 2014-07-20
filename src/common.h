68d13e38365a57e6ceb8c2fb866faf94644cc30a
Matthew Hall <mhall@mhcomputing.net>
2014-07-20 05:25:37 -0700

    common.h: fixup prototypes

--- src/common.h
+++ src/common.h
@@ -98,29 +98,29 @@ typedef struct ss_string_trie_s ss_string_trie_t;
 
 ss_re_chain_t* ss_re_chain_create(void);
 int ss_re_chain_destroy(ss_re_chain_t* re_chain);
-ss_re_entry_t* ss_re_entry_create(json_object *re_json);
+ss_re_entry_t* ss_re_entry_create(json_object* re_json);
 int ss_re_entry_destroy(ss_re_entry_t* re_entry);
-int ss_re_chain_add(ss_re_chain_t* re_chain, ss_re_entry_t *re_entry);
+int ss_re_chain_add(ss_re_chain_t* re_chain, ss_re_entry_t* re_entry);
 int ss_re_chain_remove_index(ss_re_chain_t* re_chain, int index);
-int ss_re_chain_remove_re(ss_re_chain_t* re_chain, char *re);
-int ss_re_chain_match(ss_re_chain_t* re_chain, char *input);
+int ss_re_chain_remove_re(ss_re_chain_t* re_chain, char* re);
+int ss_re_chain_match(ss_re_chain_t* re_chain, char* input);
 ss_pcap_match_t* ss_pcap_match_create(void);
 int ss_pcap_match_destroy(ss_pcap_match_t* pcap_match);
 ss_pcap_chain_t* ss_pcap_chain_create(void);
 int ss_pcap_chain_destroy(ss_pcap_chain_t* pcap_chain);
-ss_pcap_entry_t* ss_pcap_entry_create(json_object *pcap_json);
+ss_pcap_entry_t* ss_pcap_entry_create(json_object* pcap_json);
 int ss_pcap_entry_destroy(ss_pcap_entry_t* pcap_entry);
-int ss_pcap_chain_add(ss_pcap_chain_t* pcap_match, ss_pcap_entry_t *pcap_entry);
+int ss_pcap_chain_add(ss_pcap_chain_t* pcap_match, ss_pcap_entry_t* pcap_entry);
 int ss_pcap_chain_remove_index(ss_pcap_chain_t* pcap_match, int index);
-int ss_pcap_chain_remove_filter(ss_pcap_chain_t* pcap_match, char *filter);
-int ss_pcap_match_init(ss_pcap_match_t* pcap_match, struct timeval *time, uint8_t *input, unsigned int length);
-int ss_pcap_match(ss_pcap_chain_t* pcap_chain, ss_pcap_match_t *pcap_match);
-ss_cidr_table_t* ss_cidr_table_create(json_object *cidr_table_json);
+int ss_pcap_chain_remove_filter(ss_pcap_chain_t* pcap_match, char* filter);
+int ss_pcap_match_init(ss_pcap_match_t* pcap_match, struct timeval* time, uint8_t* input, unsigned int length);
+int ss_pcap_match(ss_pcap_chain_t* pcap_chain, ss_pcap_match_t* pcap_match);
+ss_cidr_table_t* ss_cidr_table_create(json_object* cidr_table_json);
 int ss_cidr_table_destroy(ss_cidr_table_t* cidr_table);
-ss_cidr_entry_t* ss_cidr_entry_create(json_object *cidr_json);
+ss_cidr_entry_t* ss_cidr_entry_create(json_object* cidr_json);
 int ss_cidr_entry_destroy(ss_cidr_entry_t* cidr_entry);
-int ss_cidr_table_add(ss_cidr_table_t* cidr_table, ss_cidr_entry_t *cidr_entry);
-int ss_cidr_table_remove(ss_cidr_table_t* cidr_table, char *cidr);
+int ss_cidr_table_add(ss_cidr_table_t* cidr_table, ss_cidr_entry_t* cidr_entry);
+int ss_cidr_table_remove(ss_cidr_table_t* cidr_table, char* cidr);
 
 /* END PROTOTYPES */
 
