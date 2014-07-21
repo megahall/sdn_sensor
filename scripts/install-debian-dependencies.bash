d2090cc2f43b5f8b65993c365bff0dbd9ee2e26d
Matthew Hall <mhall@mhcomputing.net>
2014-07-21 02:21:19 -0700

    install-debian-dependencies.bash: add more packages

--- scripts/install-debian-dependencies.bash
+++ scripts/install-debian-dependencies.bash
@@ -8,13 +8,13 @@ sudo aptitude -y purge command-not-found command-not-found-data python3-commandn
 
 sudo aptitude -y install linux-server linux-headers-server
 
-sudo aptitude -y install gawk strace ltrace telnet netcat-traditional tshark ssh rsync cproto
+sudo aptitude -y install gawk strace ltrace telnet netcat-traditional tshark ssh rsync cproto cscope
 
 sudo aptitude -y install libjson-pp-perl libperl6-slurp-perl
 
-sudo aptitude -y install build-essential libc6-dbg clang gdb-multiarch gdb-doc valgrind autoconf automake libtool git git-man manpages-dev manpages-posix-dev
+sudo aptitude -y install build-essential libc6-dbg clang gdb-multiarch gdb-doc valgrind autoconf automake libtool git git-man git-email manpages-dev manpages-posix-dev
 
-sudo aptitude -y install uthash-dev libbsd-dev libpcre3-dev zlib1g-dev libglib2.0-dev gnulib libjson-c-dev libjson-c-doc liblog4c-dev liblog4c-doc libpcap-dev
+sudo aptitude -y install uthash-dev libbsd-dev libpcre3-dev zlib1g-dev libglib2.0-dev gnulib libjson-c-dev libjson-c-doc liblog4c-dev liblog4c-doc libpcap-dev libfuse-dev
 
 # python2.7
 # sudo aptitude -y install nginx nginx-extras
