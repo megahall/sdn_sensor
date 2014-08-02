71f0185c13859383564ba8b9a1779e00d74559ca
Matthew Hall <mhall@mhcomputing.net>
2014-08-02 14:31:06 -0700

    dpdk.h: update prototypes

--- src/dpdk.h
+++ src/dpdk.h
@@ -1,13 +1,12 @@
 #ifndef __DPDK_H__
 #define __DPDK_H__
 
+#include "sdn_sensor.h"
+
 /* BEGIN PROTOTYPES */
 
-void ss_port_stats_print(struct ss_port_statistics* port_statistics, unsigned int port_count, uint32_t enabled_port_mask);
-int ss_parse_portmask(const char* portmask);
-unsigned int ss_parse_nqueue(const char* q_arg);
-int ss_parse_timer_period(const char* q_arg);
-void check_all_ports_link_status(uint8_t port_num, uint32_t port_mask);
+void ss_port_stats_print(struct ss_port_statistics* port_statistics, unsigned int port_limit);
+void ss_port_link_status_check_all(uint8_t port_limit);
 
 /* END PROTOTYPES */
 
