0b4f878ef08ce821cf6e98bb09f1b4189efcd6bb
Matthew Hall <mhall@mhcomputing.net>
2014-08-24 13:46:54 -0700

    checksum.*: req'd for ip4 and icmp{4,6} checksums

---
+++ src/checksum.c
@@ -0,0 +1,30 @@
+#include <inttypes.h>
+
+uint16_t ss_in_cksum(uint16_t* data, int len) {
+    register int sum     = 0;
+    uint16_t answer      = 0;
+    register uint16_t* w = data;
+    register int nleft   = len;
+
+    /*
+     * Our algorithm is simple, using a 32 bit accumulator (sum), we add
+     * sequential 16 bit words to it, and at the end, fold back all the
+     * carry bits from the top 16 bits into the lower 16 bits.
+     */
+    while (nleft > 1) {
+        sum += *w++;
+        nleft -= 2;
+    }
+
+    /* mop up an odd byte, if necessary */
+    if (nleft == 1) {
+        *(uint8_t*)(&answer) = *(uint8_t*) w;
+        sum += answer;
+    }
+
+    /* add back carry outs from top 16 bits to low 16 bits */
+    sum = (sum >> 16) + (sum & 0xffff); /* add hi 16 to low 16 */
+    sum += (sum >> 16);                 /* add carry */
+    answer = ~sum;                      /* truncate to 16 bits */
+    return answer;
+}
---
+++ src/checksum.h
@@ -0,0 +1,10 @@
+#ifndef __CHECKSUM_H__
+#define __CHECKSUM_H__
+
+/* BEGIN PROTOTYPES */
+
+uint16_t ss_in_cksum(uint16_t* data, int len);
+
+/* END PROTOTYPES */
+
+#endif /* __CHECKSUM_H__ */
