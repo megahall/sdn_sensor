91ae4ee6089c0f4c0f8fefab1f2e6938321324f4
Matthew Hall <mhall@mhcomputing.net>
2014-08-29 23:00:36 -0700

    ip_utils.c: fix pointer errors in usage of inet_*to*

--- src/ip_utils.h
+++ src/ip_utils.h
@@ -18,12 +18,12 @@
 
 /* BEGIN PROTOTYPES */
 
-int ss_dump_cidr(FILE* fd, const char* label, ip_addr* ip_addr);
-int ss_parse_cidr(const char* input, ip_addr* ip_addr);
-int ss_inet_pton(int af, const char* src, ip_addr* dst);
+int ss_dump_cidr(FILE* fd, const char* label, ip_addr_t* ip_addr);
+int ss_parse_cidr(const char* input, ip_addr_t* ip_addr);
+int ss_inet_pton(int af, const char* src, ip_addr_t* dst);
 int ss_inet_pton4(const char* src, uint8_t* dst);
 int ss_inet_pton6(const char* src, uint8_t* dst);
-const char* ss_inet_ntop(int af, const void* src, char* dst, unsigned int size);
+const char* ss_inet_ntop(int af, const ip_addr_t* src, char* dst, unsigned int size);
 const char* ss_inet_ntop4(const uint8_t* src, char* dst, unsigned int size);
 const char* ss_inet_ntop6(const uint8_t* src, char* dst, unsigned int size);
 
