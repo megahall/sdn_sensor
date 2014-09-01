d59f6e676190db681787ce662e32096c9b302930
Matthew Hall <mhall@mhcomputing.net>
2014-08-31 22:31:26 -0700

    ip.c: includes, metadata, self-detection, l4 detection

--- src/ip.c
+++ src/ip.c
@@ -1,8 +1,23 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+
+#include <netinet/in.h>
+#include <netinet/ip6.h>
+
+#include <rte_byteorder.h>
 #include <rte_hexdump.h>
+#include <rte_log.h>
+#include <rte_mbuf.h>
+
+#include "ip.h"
 
 #include "common.h"
-#include "ethernet.h"
 #include "sdn_sensor.h"
+#include "icmp.h"
+#include "tcp.h"
+#include "udp.h"
 
 int ss_frame_handle_ip4(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
     int rv = 0;
@@ -10,17 +25,35 @@ int ss_frame_handle_ip4(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
     rx_buf->ip4 = (ip4_hdr_t*) ((uint8_t*) rte_pktmbuf_mtod(rx_buf->mbuf, uint8_t*) + sizeof(eth_hdr_t));
     rte_memdump(stdout, "ipv4 src", &rx_buf->ip4->saddr, sizeof(rx_buf->ip4->saddr));
     rte_memdump(stdout, "ipv4 dst", &rx_buf->ip4->daddr, sizeof(rx_buf->ip4->daddr));
-    // XXX: check if this packet is for us
+    rte_memcpy(&rx_buf->data.sip, &rx_buf->ip4->saddr, sizeof(rx_buf->data.sip));
+    rte_memcpy(&rx_buf->data.dip, &rx_buf->ip4->daddr, sizeof(rx_buf->data.dip));
+    
+    rx_buf->data.self = (memcmp(&rx_buf->ip4->daddr, &ss_conf->ip4_address.ip4_addr, IPV4_ALEN)) == 0;
 
     // XXX: walk through extension headers eventually
-    printf("ip4 protocol %hhu\n", rx_buf->ip4->protocol);
+    RTE_LOG(INFO, SS, "ip4 protocol %hhu\n", rx_buf->ip4->protocol);
+    rx_buf->data.ip_protocol = rx_buf->ip4->protocol;
+    rv = ss_frame_find_l4_header(rx_buf, rx_buf->ip4->protocol);
+    if (rv) {
+        RTE_LOG(ERR, SS, "port %u received damaged ipv4 %hhu frame:\n", rx_buf->data.port_id, rx_buf->ip4->protocol);
+        rte_pktmbuf_dump(stdout, rx_buf->mbuf, rte_pktmbuf_pkt_len(rx_buf->mbuf));
+    }
+    
     switch (rx_buf->ip4->protocol) {
         case IPPROTO_ICMP: {
             rv = ss_frame_handle_icmp4(rx_buf, tx_buf);
             break;
         }
+        case IPPROTO_UDP: {
+            rv = ss_frame_handle_udp(rx_buf, tx_buf);
+            break;
+        }
+        case IPPROTO_TCP: {
+            rv = ss_frame_handle_tcp(rx_buf, tx_buf);
+            break;
+        }
         default: {
-            RTE_LOG(INFO, SS, "port %u received unsupported ipv4 0x%04hhx frame:\n", rx_buf->port_id, rx_buf->ip4->protocol);
+            RTE_LOG(INFO, SS, "port %u received unsupported ipv4 %hhu frame:\n", rx_buf->data.port_id, rx_buf->ip4->protocol);
             rte_pktmbuf_dump(stdout, rx_buf->mbuf, rte_pktmbuf_pkt_len(rx_buf->mbuf));
             rv = -1;
             break;
@@ -34,19 +67,38 @@ int ss_frame_handle_ip6(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
     int rv = 0;
 
     rx_buf->ip6 = (ip6_hdr_t*) ((uint8_t*) rte_pktmbuf_mtod(rx_buf->mbuf, uint8_t*) + sizeof(eth_hdr_t));
-    rte_memdump(stdout, "ipv6 hdr", rx_buf->ip6, sizeof(ip6_hdr_t));
+    //rte_memdump(stdout, "ipv6 hdr", rx_buf->ip6, sizeof(ip6_hdr_t));
     rte_memdump(stdout, "ipv6 src", &rx_buf->ip6->ip6_src, sizeof(rx_buf->ip6->ip6_src));
     rte_memdump(stdout, "ipv6 dst", &rx_buf->ip6->ip6_dst, sizeof(rx_buf->ip6->ip6_dst));
-    // XXX: check if this packet is for us
+    rte_memcpy(&rx_buf->data.sip, &rx_buf->ip6->ip6_src, sizeof(rx_buf->data.sip));
+    rte_memcpy(&rx_buf->data.dip, &rx_buf->ip6->ip6_dst, sizeof(rx_buf->data.dip));
 
+    rx_buf->data.self = (memcmp(&rx_buf->ip6->ip6_dst, &ss_conf->ip6_address.ip6_addr, IPV6_ALEN)) == 0;
+    
     // XXX: walk through extension headers eventually
+    RTE_LOG(INFO, SS, "ip6 protocol %hhu\n", rx_buf->ip6->ip6_nxt);
+    rx_buf->data.ip_protocol = rx_buf->ip6->ip6_nxt;
+    rv = ss_frame_find_l4_header(rx_buf, rx_buf->ip6->ip6_nxt);
+    if (rv) {
+        RTE_LOG(ERR, SS, "port %u received damaged ipv4 %hhu frame:\n", rx_buf->data.port_id, rx_buf->ip4->protocol);
+        rte_pktmbuf_dump(stdout, rx_buf->mbuf, rte_pktmbuf_pkt_len(rx_buf->mbuf));
+    }
+    
     switch (rx_buf->ip6->ip6_nxt) {
         case IPPROTO_ICMPV6: {
             rv = ss_frame_handle_icmp6(rx_buf, tx_buf);
             break;
         }
+        case IPPROTO_UDP: {
+            rv = ss_frame_handle_udp(rx_buf, tx_buf);
+            break;
+        }
+        case IPPROTO_TCP: {
+            rv = ss_frame_handle_tcp(rx_buf, tx_buf);
+            break;
+        }
         default: {
-            RTE_LOG(INFO, SS, "port %u received unsupported ipv6 0x%04hhx frame:\n", rx_buf->port_id, rx_buf->ip6->ip6_nxt);
+            RTE_LOG(INFO, SS, "port %u received unsupported ipv6 0x%04hhx frame:\n", rx_buf->data.port_id, rx_buf->ip6->ip6_nxt);
             rte_pktmbuf_dump(stdout, rx_buf->mbuf, rte_pktmbuf_pkt_len(rx_buf->mbuf));
             rv = -1;
             break;
@@ -56,34 +108,49 @@ int ss_frame_handle_ip6(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
     return rv;
 }
 
-int ss_frame_handle_ip(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
-    uint8_t protocol;
-
-    if (rx_buf->ip4) {
-        protocol = rx_buf->ip4->protocol;
+int ss_frame_find_l4_header(ss_frame_t* rx_buf, uint8_t ip_protocol) {
+    uint16_t ether_type = rte_bswap16(rx_buf->eth->ether_type);
+    
+    uint8_t* l3_pointer;
+    size_t   l3_size;
+    
+    switch (ether_type) {
+        case ETHER_TYPE_IPV4: {
+            l3_pointer = (uint8_t*) rx_buf->ip4;
+            l3_size = sizeof(ip4_hdr_t);
+            break;
+        }
+        case ETHER_TYPE_IPV6: {
+            l3_pointer = (uint8_t*) rx_buf->ip6;
+            l3_size = sizeof(ip6_hdr_t);
+            break;
+        }
+        default: {
+            RTE_LOG(ERR, SS, "could not locate l4 header for ether type 0x%04hx\n", ether_type);
+            return -1;
+        }
     }
-    else if (rx_buf->ip6) {
-        protocol = rx_buf->ip6->ip6_nxt;
+    
+    switch (ip_protocol) {
+        case IPPROTO_ICMP: {
+            rx_buf->icmp4 = (icmp4_hdr_t*) (l3_pointer + l3_size);
+            return 0;
+        }
+        case IPPROTO_ICMPV6: {
+            rx_buf->icmp6 = (icmp6_hdr_t*) (l3_pointer + l3_size);
+            return 0;
+        }
+        case IPPROTO_UDP: {
+            rx_buf->udp   = (udp_hdr_t*) (l3_pointer + l3_size);
+            return 0;
+        }
+        case IPPROTO_TCP: {
+            rx_buf->tcp   = (tcp_hdr_t*) (l3_pointer + l3_size);
+            return 0;
+        }
+        default: {
+            RTE_LOG(ERR, SS, "could not locate l4 header for ip protocol %hhd\n", ip_protocol);
+            return -1;
+        }
     }
-    else {
-        RTE_LOG(ERR, SS, "unknown IP packet with EtherType %02hx\n", rx_buf->eth->ether_type);
-        return -1;
-    }
-
-    switch (protocol) {
-        // ICMP
-        // IGMP
-        // TCP
-        // UDP
-        // DCCP
-        // IPv4 tunneled, IPv6 tunneled
-        // GRE
-        // ICMPv6
-        // ESP
-        // AH
-        // SCTP
-    }
-
-    return 0;
 }
-
