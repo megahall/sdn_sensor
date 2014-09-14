c8b4015f4bb497ba4ac26f4ab55096f0ddc0d1b0
Matthew Hall <mhall@mhcomputing.net>
2014-09-14 02:33:23 -0700

    nn_queue.c: move metadata functions to metadata.{c,h}

--- src/nn_queue.c
+++ src/nn_queue.c
@@ -18,6 +18,8 @@
 #include <nanomsg/survey.h>
 #include <nanomsg/bus.h>
 
+#include <rte_log.h>
+
 #include <pcap/pcap.h>
 
 /* XXX: mhall: causes include loop w/ common.h */
@@ -28,6 +30,8 @@
 #include "sdn_sensor.h"
 
 int ss_nn_queue_create(json_object* items, nn_queue_t* nn_queue) {
+    // int rv;
+    int so_value;
     char* value;
     
     memset(nn_queue, 0, sizeof(nn_queue_t));
@@ -75,6 +79,14 @@ int ss_nn_queue_create(json_object* items, nn_queue_t* nn_queue) {
         fprintf(stderr, "could not allocate nm queue socket: %s\n", nn_strerror(nn_errno()));
         goto error_out;
     }
+    so_value = 0;
+    /*
+    rv = nn_setsockopt(nn_queue->conn, NN_SOL_SOCKET, NN_IPV4ONLY, &so_value, sizeof(so_value));
+    if (rv != 0) {
+        fprintf(stderr, "could not enable nm ipv6 support: %s\n", nn_strerror(nn_errno()));
+        goto error_out;
+    }
+    */
     nn_queue->remote_id = nn_connect(nn_queue->conn, nn_queue->url);
     if (nn_queue->remote_id < 0) {
         fprintf(stderr, "could not connect nm queue socket: %s\n", nn_strerror(nn_errno()));
@@ -142,124 +154,12 @@ int ss_nn_queue_dump(nn_queue_t* nn_queue) {
     return 0;
 }
 
-uint8_t* ss_nn_queue_prepare_metadata(const char* source, nn_queue_t* nn_queue, ss_frame_t* fbuf) {
-    char tmp[1024];
-    uint8_t* rv;
-    
-    if (nn_queue->format != NN_FORMAT_METADATA) {
-        fprintf(stderr, "format %d not supported yet\n", nn_queue->format);
-        goto error_out;
-    }
-    
-    fbuf->data.json = json_object_new_object();
-    if (fbuf->data.json == NULL) {
-        fprintf(stderr, "could not allocate json object\n");
-        goto error_out;
-    }
-    
-    json_object* jsource     = json_object_new_string(source);
-    
-    json_object* port_id     = json_object_new_int(fbuf->data.port_id);
-    if (port_id == NULL) goto error_out;
-    json_object* direction   = json_object_new_int(fbuf->data.direction);
-    if (direction == NULL) goto error_out;
-    json_object* self        = json_object_new_int(fbuf->data.self);
-    if (self == NULL) goto error_out;
-    json_object* length      = json_object_new_int(fbuf->data.length);
-    if (length == NULL) goto error_out;
-    
-    snprintf(tmp, sizeof(tmp), "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx",
-        fbuf->data.smac[0], fbuf->data.smac[1], fbuf->data.smac[2],
-        fbuf->data.smac[3], fbuf->data.smac[4], fbuf->data.smac[5]);
-    json_object* smac        = json_object_new_string(tmp);
-    if (smac == NULL) goto error_out;
-    
-    snprintf(tmp, sizeof(tmp), "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx",
-        fbuf->data.dmac[0], fbuf->data.dmac[1], fbuf->data.dmac[2],
-        fbuf->data.dmac[3], fbuf->data.dmac[4], fbuf->data.dmac[5]);
-    json_object* dmac        = json_object_new_string(tmp);
-    if (dmac == NULL) goto error_out;
-    
-    json_object* eth_type    = json_object_new_int(fbuf->data.eth_type);
-    if (eth_type == NULL) goto error_out;
-    
-    json_object* sip = NULL;
-    json_object* dip = NULL;
-    if      (fbuf->data.eth_type == ETHER_TYPE_IPV4) {
-        snprintf(tmp, sizeof(tmp), "%hhu.%hhu.%hhu.%hhu",
-            fbuf->data.sip[0], fbuf->data.sip[1], fbuf->data.sip[2], fbuf->data.sip[3]);
-        sip = json_object_new_string(tmp);
-        snprintf(tmp, sizeof(tmp), "%hhu.%hhu.%hhu.%hhu",
-            fbuf->data.dip[0], fbuf->data.dip[1], fbuf->data.dip[2], fbuf->data.dip[3]);
-        dip = json_object_new_string(tmp);
-    }
-    else if (fbuf->data.eth_type == ETHER_TYPE_IPV6) {
-        snprintf(tmp, sizeof(tmp), "%hx:%hx:%hx:%hx:%hx:%hx:%hx:%hx",
-            *(uint16_t*) &fbuf->data.sip[0],  *(uint16_t*) &fbuf->data.sip[2],
-            *(uint16_t*) &fbuf->data.sip[4],  *(uint16_t*) &fbuf->data.sip[6],
-            *(uint16_t*) &fbuf->data.sip[8],  *(uint16_t*) &fbuf->data.sip[10],
-            *(uint16_t*) &fbuf->data.sip[12], *(uint16_t*) &fbuf->data.sip[14]);
-        sip = json_object_new_string(tmp);
-        snprintf(tmp, sizeof(tmp), "%hx:%hx:%hx:%hx:%hx:%hx:%hx:%hx",
-            *(uint16_t*) &fbuf->data.sip[0],  *(uint16_t*) &fbuf->data.sip[2],
-            *(uint16_t*) &fbuf->data.sip[4],  *(uint16_t*) &fbuf->data.sip[6],
-            *(uint16_t*) &fbuf->data.sip[8],  *(uint16_t*) &fbuf->data.sip[10],
-            *(uint16_t*) &fbuf->data.sip[12], *(uint16_t*) &fbuf->data.sip[14]);
-        dip = json_object_new_string(tmp);
-    }
-    else {
-        fprintf(stderr, "could not extract IP addresses\n");
-    }
-    
-    json_object* ip_protocol = json_object_new_int(fbuf->data.ip_protocol);
-    if (ip_protocol == NULL) goto error_out;
-    json_object* ttl         = json_object_new_int(fbuf->data.ttl);
-    if (ttl == NULL) goto error_out;
-    json_object* l4_length   = json_object_new_int(fbuf->data.l4_length);
-    if (l4_length == NULL) goto error_out;
-    json_object* icmp_type   = json_object_new_int(fbuf->data.icmp_type);
-    if (icmp_type == NULL) goto error_out;
-    json_object* icmp_code   = json_object_new_int(fbuf->data.icmp_code);
-    if (icmp_code == NULL) goto error_out;
-    json_object* sport       = json_object_new_int(fbuf->data.sport);
-    if (sport == NULL) goto error_out;
-    json_object* dport       = json_object_new_int(fbuf->data.dport);
-    if (dport == NULL) goto error_out;
-    
-    json_object* dns_name    = json_object_new_string((char*)fbuf->data.dns_name);
-    if (dns_name == NULL) goto error_out;
-    
-    // XXX: add support for dns_values field
-    
-    json_object_object_add(fbuf->data.json, "source",      jsource);
-    json_object_object_add(fbuf->data.json, "port_id",     port_id);
-    json_object_object_add(fbuf->data.json, "direction",   direction);
-    json_object_object_add(fbuf->data.json, "self",        self);
-    json_object_object_add(fbuf->data.json, "length",      length);
-    json_object_object_add(fbuf->data.json, "smac",        smac);
-    json_object_object_add(fbuf->data.json, "dmac",        dmac);
-    json_object_object_add(fbuf->data.json, "eth_type",    eth_type);
-    json_object_object_add(fbuf->data.json, "sip",         sip);
-    json_object_object_add(fbuf->data.json, "dip",         dip);
-    json_object_object_add(fbuf->data.json, "ip_protocol", ip_protocol);
-    json_object_object_add(fbuf->data.json, "ttl",         ttl);
-    json_object_object_add(fbuf->data.json, "l4_length",   l4_length);
-    json_object_object_add(fbuf->data.json, "icmp_type",   icmp_type);
-    json_object_object_add(fbuf->data.json, "icmp_code",   icmp_code);
-    json_object_object_add(fbuf->data.json, "sport",       sport);
-    json_object_object_add(fbuf->data.json, "dport",       dport);
-    json_object_object_add(fbuf->data.json, "dns_name",    dns_name);
-    
-    rv = (uint8_t*) json_object_to_json_string_ext(fbuf->data.json, JSON_C_TO_STRING_SPACED);
-    return rv;
-    
-    error_out:
-    fprintf(stderr, "could not serialize packet metadata\n");
-    return NULL;
-}
-
 int ss_nn_queue_send(nn_queue_t* nn_queue, uint8_t* message, uint16_t length) {
     int rv = 0;
+    
+    // XXX: assume message is a C string for now
+    RTE_LOG(NOTICE, SS, "nn_queue %s: message id %014lu: %s\n",
+        nn_queue->url, nn_queue->tx_messages, message);
     
     rv = nn_send(nn_queue->conn, message, length, NN_DONTWAIT);
     
