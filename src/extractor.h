6d703b765747e67fe2b1cb74100ad2435aa79e4f
Matthew Hall <mhall@mhcomputing.net>
2014-09-14 02:30:18 -0700

    extractor.h: prototypes

--- src/extractor.h
+++ src/extractor.h
@@ -3,10 +3,13 @@
 
 #include "common.h"
 
+#include "dns.h"
+
 /* BEGIN PROTOTYPES */
 
 int ss_extract_eth(ss_frame_t* fbuf);
 int ss_extract_dns(ss_frame_t* fbuf);
+int ss_extract_dns_atype(ss_answer_t* result, dns_answer_t* aptr);
 
 /* END PROTOTYPES */
 
