d74925e1b6a0cec648532340edb757ea1b2643f9
Matthew Hall <mhall@mhcomputing.net>
2014-09-28 16:53:10 -0700

    json.h: prototypes

--- src/json.h
+++ src/json.h
@@ -1,10 +1,14 @@
 #ifndef __JSON_H__
 #define __JSON_H__
 
+#include <json-c/json.h>
+#include <json-c/json_object_private.h>
 
 /* BEGIN PROTOTYPES */
 
+const char* ss_json_string_view(json_object* items, const char* key);
 char* ss_json_string_get(json_object* items, const char* key);
+int ss_json_boolean_get(json_object* items, const char* key, int vdefault);
 
 /* END PROTOTYPES */
 
