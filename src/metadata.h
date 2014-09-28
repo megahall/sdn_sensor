f0b427d7e36011c5cfe8ae66113165b32e6e4d69
Matthew Hall <mhall@mhcomputing.net>
2014-09-28 16:53:53 -0700

    metadata.h: prototypes

--- src/metadata.h
+++ src/metadata.h
@@ -2,6 +2,7 @@
 #define __METADATA_H__
 
 #include <json-c/json.h>
+#include <json-c/json_object_private.h>
 
 #include "common.h"
 #include "ioc.h"
@@ -9,8 +10,11 @@
 
 /* BEGIN PROTOTYPES */
 
-uint8_t* ss_nn_queue_prepare_metadata(const char* source, nn_queue_t* nn_queue, ss_frame_t* fbuf, ss_ioc_entry_t* iptr);
-json_object* ss_ioc_prepare_metadata(const char* source, nn_queue_t* nn_queue, ss_ioc_entry_t* iptr, json_object* json);
+int ss_metadata_prepare_eth(const char* source, nn_queue_t* nn_queue, json_object* jobject, ss_frame_t* fbuf);
+int ss_metadata_prepare_ip(const char* source, nn_queue_t* nn_queue, json_object* jobject, ss_frame_t* fbuf);
+int ss_metadata_prepare_ioc(const char* source, nn_queue_t* nn_queue, ss_ioc_entry_t* iptr, json_object* json);
+uint8_t* ss_metadata_prepare_frame(const char* source, nn_queue_t* nn_queue, ss_frame_t* fbuf, ss_ioc_entry_t* iptr);
+uint8_t* ss_metadata_prepare_syslog(const char* source, nn_queue_t* nn_queue, ss_frame_t* fbuf, ss_ioc_entry_t* iptr, uint8_t* message, int length);
 
 /* END PROTOTYPES */
 
