0aef6d23a8b67ebea5f549cb02ea303f3021dcbf
Matthew Hall <mhall@mhcomputing.net>
2014-09-28 16:53:19 -0700

    metadata.c: split submetadata to separate functions, add syslog

--- src/metadata.c
+++ src/metadata.c
@@ -11,49 +11,80 @@
 #include "json.h"
 #include "nn_queue.h"
 
-uint8_t* ss_nn_queue_prepare_metadata(const char* source, nn_queue_t* nn_queue, ss_frame_t* fbuf, ss_ioc_entry_t* iptr) {
+int ss_metadata_prepare_eth(const char* source, nn_queue_t* nn_queue, json_object* jobject, ss_frame_t* fbuf) {
     char tmp[1024];
-    uint8_t* rv;
+    json_object* jsource   = NULL;
+    json_object* port_id   = NULL;
+    json_object* direction = NULL;
+    json_object* self      = NULL;
+    json_object* length    = NULL;
+    json_object* eth_type  = NULL;
+    json_object* smac      = NULL;
+    json_object* dmac      = NULL;
     
-    if (nn_queue->format != NN_FORMAT_METADATA) {
-        fprintf(stderr, "format %d not supported yet\n", nn_queue->format);
-        goto error_out;
-    }
-    
-    fbuf->data.json = json_object_new_object();
-    if (fbuf->data.json == NULL) {
-        fprintf(stderr, "could not allocate json object\n");
-        goto error_out;
-    }
-    
-    json_object* jsource     = json_object_new_string(source);
-    
-    json_object* port_id     = json_object_new_int(fbuf->data.port_id);
+    jsource     = json_object_new_string(source);
+    if (jsource == NULL) goto error_out;
+    port_id     = json_object_new_int(fbuf->data.port_id);
     if (port_id == NULL) goto error_out;
-    json_object* direction   = json_object_new_int(fbuf->data.direction);
+    direction   = json_object_new_int(fbuf->data.direction);
     if (direction == NULL) goto error_out;
-    json_object* self        = json_object_new_int(fbuf->data.self);
+    self        = json_object_new_int(fbuf->data.self);
     if (self == NULL) goto error_out;
-    json_object* length      = json_object_new_int(fbuf->data.length);
+    length      = json_object_new_int(fbuf->data.length);
     if (length == NULL) goto error_out;
-    
+    eth_type    = json_object_new_int(fbuf->data.eth_type);
+    if (eth_type == NULL) goto error_out;
+
     snprintf(tmp, sizeof(tmp), "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx",
         fbuf->data.smac[0], fbuf->data.smac[1], fbuf->data.smac[2],
         fbuf->data.smac[3], fbuf->data.smac[4], fbuf->data.smac[5]);
-    json_object* smac        = json_object_new_string(tmp);
+    smac        = json_object_new_string(tmp);
     if (smac == NULL) goto error_out;
     
     snprintf(tmp, sizeof(tmp), "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx",
         fbuf->data.dmac[0], fbuf->data.dmac[1], fbuf->data.dmac[2],
         fbuf->data.dmac[3], fbuf->data.dmac[4], fbuf->data.dmac[5]);
-    json_object* dmac        = json_object_new_string(tmp);
+    dmac        = json_object_new_string(tmp);
     if (dmac == NULL) goto error_out;
+        
+    json_object_object_add(jobject, "source",    jsource);
+    json_object_object_add(jobject, "port_id",   port_id);
+    json_object_object_add(jobject, "direction", direction);
+    json_object_object_add(jobject, "self",      self);
+    json_object_object_add(jobject, "length",    length);
+    json_object_object_add(jobject, "eth_type",  eth_type);
+    json_object_object_add(jobject, "smac",      smac);
+    json_object_object_add(jobject, "dmac",      dmac);
     
-    json_object* eth_type    = json_object_new_int(fbuf->data.eth_type);
-    if (eth_type == NULL) goto error_out;
+    return 0;
     
-    json_object* sip = NULL;
-    json_object* dip = NULL;
+    error_out:
+    fprintf(stderr, "could not serialize ethernet metadata\n");
+    if (jsource)   json_object_put(jsource);
+    if (port_id)   json_object_put(port_id);
+    if (direction) json_object_put(direction);
+    if (self)      json_object_put(self);
+    if (length)    json_object_put(length);
+    if (smac)      json_object_put(smac);
+    if (dmac)      json_object_put(dmac);
+    if (eth_type)  json_object_put(eth_type);
+    
+    return -1;
+}
+
+int ss_metadata_prepare_ip(const char* source, nn_queue_t* nn_queue, json_object* jobject, ss_frame_t* fbuf) {
+    char tmp[1024];
+    json_object* sip         = NULL;
+    json_object* dip         = NULL;
+    json_object* ip_protocol = NULL;
+    json_object* ttl         = NULL;
+    json_object* l4_length   = NULL;
+    json_object* icmp_type   = NULL;
+    json_object* icmp_code   = NULL;
+    json_object* sport       = NULL;
+    json_object* dport       = NULL;
+    json_object* dns_name    = NULL;
+    
     if      (fbuf->data.eth_type == ETHER_TYPE_IPV4) {
         snprintf(tmp, sizeof(tmp), "%hhu.%hhu.%hhu.%hhu",
             fbuf->data.sip[0], fbuf->data.sip[1], fbuf->data.sip[2], fbuf->data.sip[3]);
@@ -80,93 +111,88 @@ uint8_t* ss_nn_queue_prepare_metadata(const char* source, nn_queue_t* nn_queue, 
         fprintf(stderr, "could not extract IP addresses\n");
     }
     
-    json_object* ip_protocol = json_object_new_int(fbuf->data.ip_protocol);
+    ip_protocol = json_object_new_int(fbuf->data.ip_protocol);
     if (ip_protocol == NULL) goto error_out;
-    json_object* ttl         = json_object_new_int(fbuf->data.ttl);
+    ttl         = json_object_new_int(fbuf->data.ttl);
     if (ttl == NULL) goto error_out;
-    json_object* l4_length   = json_object_new_int(fbuf->data.l4_length);
+    l4_length   = json_object_new_int(fbuf->data.l4_length);
     if (l4_length == NULL) goto error_out;
-    json_object* icmp_type   = json_object_new_int(fbuf->data.icmp_type);
+    icmp_type   = json_object_new_int(fbuf->data.icmp_type);
     if (icmp_type == NULL) goto error_out;
-    json_object* icmp_code   = json_object_new_int(fbuf->data.icmp_code);
+    icmp_code   = json_object_new_int(fbuf->data.icmp_code);
     if (icmp_code == NULL) goto error_out;
-    json_object* sport       = json_object_new_int(fbuf->data.sport);
+    sport       = json_object_new_int(fbuf->data.sport);
     if (sport == NULL) goto error_out;
-    json_object* dport       = json_object_new_int(fbuf->data.dport);
+    dport       = json_object_new_int(fbuf->data.dport);
     if (dport == NULL) goto error_out;
-    
-    json_object* dns_name    = json_object_new_string((char*)fbuf->data.dns_name);
-    if (dns_name == NULL) goto error_out;
-    
+    dns_name    = json_object_new_string((char*)fbuf->data.dns_name);
+    if (dns_name == NULL) goto error_out;    
+
+    json_object_object_add(jobject, "sip",         sip);
+    json_object_object_add(jobject, "dip",         dip);
+    json_object_object_add(jobject, "ip_protocol", ip_protocol);
+    json_object_object_add(jobject, "ttl",         ttl);
+    json_object_object_add(jobject, "l4_length",   l4_length);
+    json_object_object_add(jobject, "icmp_type",   icmp_type);
+    json_object_object_add(jobject, "icmp_code",   icmp_code);
+    json_object_object_add(jobject, "sport",       sport);
+    json_object_object_add(jobject, "dport",       dport);
+    json_object_object_add(jobject, "dns_name",    dns_name);
     // XXX: add support for dns_answers field, dns query type
     
-    json_object_object_add(fbuf->data.json, "source",      jsource);
-    json_object_object_add(fbuf->data.json, "port_id",     port_id);
-    json_object_object_add(fbuf->data.json, "direction",   direction);
-    json_object_object_add(fbuf->data.json, "self",        self);
-    json_object_object_add(fbuf->data.json, "length",      length);
-    json_object_object_add(fbuf->data.json, "smac",        smac);
-    json_object_object_add(fbuf->data.json, "dmac",        dmac);
-    json_object_object_add(fbuf->data.json, "eth_type",    eth_type);
-    json_object_object_add(fbuf->data.json, "sip",         sip);
-    json_object_object_add(fbuf->data.json, "dip",         dip);
-    json_object_object_add(fbuf->data.json, "ip_protocol", ip_protocol);
-    json_object_object_add(fbuf->data.json, "ttl",         ttl);
-    json_object_object_add(fbuf->data.json, "l4_length",   l4_length);
-    json_object_object_add(fbuf->data.json, "icmp_type",   icmp_type);
-    json_object_object_add(fbuf->data.json, "icmp_code",   icmp_code);
-    json_object_object_add(fbuf->data.json, "sport",       sport);
-    json_object_object_add(fbuf->data.json, "dport",       dport);
-    json_object_object_add(fbuf->data.json, "dns_name",    dns_name);
-    
-    if (iptr) {
-        ss_ioc_prepare_metadata(source, nn_queue, iptr, fbuf->data.json);
-    }
-    
-    rv = (uint8_t*) json_object_to_json_string_ext(fbuf->data.json, JSON_C_TO_STRING_SPACED);
-    return rv;
+    return 0;
     
     error_out:
-    fprintf(stderr, "could not serialize packet metadata\n");
-    return NULL;
+    fprintf(stderr, "could not serialize ethernet metadata\n");
+    if (sip)         json_object_put(sip);
+    if (dip)         json_object_put(dip);
+    if (ip_protocol) json_object_put(ip_protocol);
+    if (ttl)         json_object_put(ttl);
+    if (l4_length)   json_object_put(l4_length);
+    if (icmp_type)   json_object_put(icmp_type);
+    if (icmp_code)   json_object_put(icmp_code);
+    if (sport)       json_object_put(sport);
+    if (dport)       json_object_put(dport);
+    if (dns_name)    json_object_put(dns_name);
+    
+    return -1;
 }
 
-json_object* ss_ioc_prepare_metadata(const char* source, nn_queue_t* nn_queue, ss_ioc_entry_t* iptr, json_object* json) {
+int ss_metadata_prepare_ioc(const char* source, nn_queue_t* nn_queue, ss_ioc_entry_t* iptr, json_object* json) {
     char ip_str[SS_ADDR_STR_MAX];
     const char* result;
-
+    
+    json_object* file_id     = NULL;
+    json_object* ioc_id      = NULL;
+    json_object* type        = NULL;
+    json_object* threat_type = NULL;
+    json_object* ip          = NULL;
+    json_object* value       = NULL;
+    json_object* dns         = NULL;
+    
     if (nn_queue->format != NN_FORMAT_METADATA) {
         fprintf(stderr, "format %d not supported yet\n", nn_queue->format);
         goto error_out;
     }
-
-    if (json == NULL) {
-        json = json_object_new_object();
-    }
-    if (json == NULL) {
-        fprintf(stderr, "could not allocate json object\n");
-        goto error_out;
-    }
-
-    json_object* file_id     = json_object_new_int(iptr->file_id);
+    
+    file_id     = json_object_new_int(iptr->file_id);
     if (file_id == NULL) goto error_out;
-    json_object* ioc_id      = json_object_new_int(iptr->id);
+    ioc_id      = json_object_new_int(iptr->id);
     if (ioc_id == NULL) goto error_out;
-    json_object* type        = json_object_new_string(ss_ioc_type_dump(iptr->type));
+    type        = json_object_new_string(ss_ioc_type_dump(iptr->type));
     if (type == NULL) goto error_out;
-    json_object* threat_type = json_object_new_string(iptr->threat_type);
+    threat_type = json_object_new_string(iptr->threat_type);
     if (threat_type == NULL) goto error_out;
     result = ss_inet_ntop(&iptr->ip, ip_str, sizeof(ip_str));
-    json_object* ip          = NULL;
     if (result != NULL) {
         ip = json_object_new_string(ip_str);
     }
     if (ip == NULL) goto error_out;
-    json_object* value       = json_object_new_string(iptr->value);
+    value       = json_object_new_string(iptr->value);
     if (value == NULL) goto error_out;
-    json_object* dns         = json_object_new_string(iptr->dns);
+    dns         = json_object_new_string(iptr->dns);
     if (dns == NULL) goto error_out;
-
+    
     json_object_object_add(json, "file_id",     file_id);
     json_object_object_add(json, "ioc_id",      ioc_id);
     json_object_object_add(json, "type",        type);
@@ -174,10 +200,96 @@ json_object* ss_ioc_prepare_metadata(const char* source, nn_queue_t* nn_queue, s
     json_object_object_add(json, "ip",          ip);
     json_object_object_add(json, "value",       value);
     json_object_object_add(json, "dns",         dns);
-
-    return json;
-
+    
+    return 0;
+    
     error_out:
     fprintf(stderr, "could not serialize ioc id: %lu\n", iptr->id);
+    if (file_id)     json_object_put(file_id);
+    if (ioc_id)      json_object_put(ioc_id);
+    if (type)        json_object_put(type);
+    if (threat_type) json_object_put(threat_type);
+    if (ip)          json_object_put(ip);
+    if (value)       json_object_put(value);
+    if (dns)         json_object_put(dns);
+    
+    return -1;
+}
+
+uint8_t* ss_metadata_prepare_frame(const char* source, nn_queue_t* nn_queue, ss_frame_t* fbuf, ss_ioc_entry_t* iptr) {
+    int          irv;
+    uint8_t*     rv;
+    json_object* jobject = NULL;
+    
+    if (nn_queue->format != NN_FORMAT_METADATA) {
+        fprintf(stderr, "format %d not supported yet\n", nn_queue->format);
+        goto error_out;
+    }
+    
+    jobject = json_object_new_object();
+    if (jobject == NULL) {
+        fprintf(stderr, "could not allocate json object\n");
+        goto error_out;
+    }
+    
+    irv = ss_metadata_prepare_eth(source, nn_queue, jobject, fbuf);
+    if (irv) goto error_out;
+    
+    irv = ss_metadata_prepare_ip(source, nn_queue, jobject, fbuf);
+    if (irv) goto error_out;
+    
+    if (iptr) {
+        irv = ss_metadata_prepare_ioc(source, nn_queue, iptr, jobject);
+        if (irv) goto error_out;
+    }
+    
+    rv = (uint8_t*) json_object_to_json_string_ext(jobject, JSON_C_TO_STRING_SPACED);
+    json_object_put(jobject);
+    return rv;
+    
+    error_out:
+    fprintf(stderr, "could not serialize packet metadata\n");
+    return NULL;
+}
+
+uint8_t* ss_metadata_prepare_syslog(const char* source, nn_queue_t* nn_queue, ss_frame_t* fbuf, ss_ioc_entry_t* iptr, uint8_t* message, int length) {
+    int          irv;
+    uint8_t*     rv;
+    json_object* jmessage = NULL;
+    json_object* jobject = NULL;
+    
+    if (nn_queue->format != NN_FORMAT_METADATA) {
+        fprintf(stderr, "format %d not supported yet\n", nn_queue->format);
+        goto error_out;
+    }
+    
+    jobject = json_object_new_object();
+    if (jobject == NULL) {
+        fprintf(stderr, "could not allocate json object\n");
+        goto error_out;
+    }
+    
+    irv = ss_metadata_prepare_ip(source, nn_queue, jobject, fbuf);
+    if (irv) goto error_out;
+    
+    if (iptr) {
+        irv = ss_metadata_prepare_ioc(source, nn_queue, iptr, jobject);
+        if (irv) goto error_out;
+    }
+    
+    // XXX: for now assume the message is C char*
+    jmessage = json_object_new_string((char*) message);
+    if (jmessage == NULL) goto error_out;
+    json_object_object_add(jobject, "message", jmessage);
+    
+    rv = (uint8_t*) json_object_to_json_string_ext(jobject, JSON_C_TO_STRING_SPACED);
+    json_object_put(jobject);
+    return rv;
+    
+    error_out:
+    fprintf(stderr, "could not create syslog metadata\n");
+    if (jmessage) json_object_put(jmessage);
+    if (jobject)  json_object_put(jobject);
+    
     return NULL;
 }
