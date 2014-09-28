40ece0ce74ae92c02d1db5cbd07e0cdc05d35946
Matthew Hall <mhall@mhcomputing.net>
2014-09-28 16:52:42 -0700

    json.c: add string_view, fix string_get, add boolean_get

--- src/json.c
+++ src/json.c
@@ -3,30 +3,67 @@
 #include <strings.h>
 
 #include <json-c/json.h>
+#include <json-c/json_object_private.h>
 
-char* ss_json_string_get(json_object* items, const char* key) {
+#include "je_utils.h"
+
+const char* ss_json_string_view(json_object* items, const char* key) {
     json_object* item;
-    const char* value;
-    char* rv;
+    const char* value = NULL;
     
     item = json_object_object_get(items, key);
     if (item == NULL) {
         fprintf(stderr, "key %s not present\n", key);
-        return NULL;
+        goto out;
     }
     if (!json_object_is_type(item, json_type_string)) {
-        fprintf(stderr, "value for %s is not object\n", key);
-        return NULL;
+        fprintf(stderr, "value for %s is not string\n", key);
+        goto out;
     }
     value = json_object_get_string(item);
     if (value == NULL) {
         fprintf(stderr, "value for %s is null\n", key);
-        return NULL;
+        goto out;
     }
-    rv = strdup(value);
+    
+    out:
+    return value;
+}
+
+char* ss_json_string_get(json_object* items, const char* key) {
+    const char* value;
+    char* rv = NULL;
+    
+    value = ss_json_string_view(items, key);
+    if (value == NULL) {
+        fprintf(stderr, "value for %s is null\n", key);
+        goto out;
+    }
+    rv = je_strdup(value);
     if (rv == NULL) {
         fprintf(stderr, "could not allocate return value for %s\n", key);
-        return NULL;
+        goto out;
     }
+    
+    out:
+    return rv;
+}
+
+int ss_json_boolean_get(json_object* items, const char* key, int vdefault) {
+    json_object* item;
+    int rv = vdefault;
+    
+    item = json_object_object_get(items, key);
+    if (item == NULL) {
+        fprintf(stderr, "key %s not present\n", key);
+        goto out;
+    }
+    if (!json_object_is_type(item, json_type_boolean)) {
+        fprintf(stderr, "value for %s is not boolean\n", key);
+        goto out;
+    }
+    rv = json_object_get_boolean(item);
+    
+    out:
     return rv;
 }
