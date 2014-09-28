7672e1ca779eae4f149273cfd4648f296d8158d9
Matthew Hall <mhall@mhcomputing.net>
2014-09-28 16:48:20 -0700

    ethernet.c: adjust log type, some log levels

--- src/ethernet.c
+++ src/ethernet.c
@@ -37,15 +37,15 @@ void ss_frame_handle(struct rte_mbuf* mbuf, unsigned int port_id) {
     rx_buf.data.length    = rte_pktmbuf_pkt_len(mbuf);
     
     if (rx_buf.data.length < sizeof(eth_hdr_t)) {
-        RTE_LOG(ERR, SS, "received runt Ethernet frame of length %u:\n", rx_buf.data.length);
+        RTE_LOG(ERR, STACK, "received runt Ethernet frame of length %u:\n", rx_buf.data.length);
         rte_pktmbuf_dump(stderr, mbuf, rte_pktmbuf_pkt_len(mbuf));
         goto out;
     }
     rx_buf.eth = rte_pktmbuf_mtod(mbuf, eth_hdr_t*);
-    RTE_LOG(INFO, SS, "eth src %02x:%02x:%02x:%02x:%02x:%02x\n",
+    RTE_LOG(INFO, STACK, "eth src %02x:%02x:%02x:%02x:%02x:%02x\n",
         rx_buf.eth->s_addr.addr_bytes[0], rx_buf.eth->s_addr.addr_bytes[1], rx_buf.eth->s_addr.addr_bytes[2],
         rx_buf.eth->s_addr.addr_bytes[3], rx_buf.eth->s_addr.addr_bytes[4], rx_buf.eth->s_addr.addr_bytes[5]);
-    RTE_LOG(INFO, SS, "eth dst %02x:%02x:%02x:%02x:%02x:%02x\n",
+    RTE_LOG(INFO, STACK, "eth dst %02x:%02x:%02x:%02x:%02x:%02x\n",
         rx_buf.eth->d_addr.addr_bytes[0], rx_buf.eth->d_addr.addr_bytes[1], rx_buf.eth->d_addr.addr_bytes[2],
         rx_buf.eth->d_addr.addr_bytes[3], rx_buf.eth->d_addr.addr_bytes[4], rx_buf.eth->d_addr.addr_bytes[5]);
     rte_memcpy(&rx_buf.data.smac, &rx_buf.eth->s_addr, sizeof(rx_buf.data.smac));
@@ -54,18 +54,19 @@ void ss_frame_handle(struct rte_mbuf* mbuf, unsigned int port_id) {
     uint16_t ether_type = rte_bswap16(rx_buf.eth->ether_type);
     rx_buf.data.eth_type = ether_type;
     if (ether_type == ETHER_TYPE_VLAN) {
-        RTE_LOG(NOTICE, SS, "port %u attempting decode of VLAN frame\n", port_id);
-        rte_pktmbuf_dump(stderr, mbuf, rte_pktmbuf_pkt_len(mbuf));
+        RTE_LOG(NOTICE, STACK, "port %u attempting decode of VLAN frame\n", port_id);
+        if (rte_get_log_level() >= RTE_LOG_DEBUG)
+            rte_pktmbuf_dump(stderr, mbuf, rte_pktmbuf_pkt_len(mbuf));
         rx_buf.ethv = rte_pktmbuf_mtod(mbuf, eth_vhdr_t*);
         rx_buf.eth = (eth_hdr_t*) ((uint8_t*) rx_buf.eth + sizeof(eth_vhdr_t));
     }
     
-    RTE_LOG(INFO, SS, "process frame type 0x%04hx size %u\n", ether_type, rte_pktmbuf_pkt_len(mbuf));
+    RTE_LOG(INFO, STACK, "process frame type 0x%04hx size %u\n", ether_type, rte_pktmbuf_pkt_len(mbuf));
     
     switch (ether_type) {
         /*
         case ETHER_TYPE_VLAN: {
-            RTE_LOG(INFO, SS, "port %u received unsupported VLAN frame:\n", port_id);
+            RTE_LOG(INFO, STACK, "port %u received unsupported VLAN frame:\n", port_id);
             rte_pktmbuf_dump(stderr, mbuf, rte_pktmbuf_pkt_len(mbuf));
             break;
         }
@@ -76,7 +77,7 @@ void ss_frame_handle(struct rte_mbuf* mbuf, unsigned int port_id) {
         }
         case ETHER_TYPE_IPV4: {
             if (rx_buf.data.length < sizeof(eth_hdr_t) + sizeof(ip4_hdr_t)) {
-                RTE_LOG(ERR, SS, "received runt IPv4 frame of length %u:\n", rx_buf.data.length);
+                RTE_LOG(ERR, STACK, "received runt IPv4 frame of length %u:\n", rx_buf.data.length);
                 rte_pktmbuf_dump(stderr, mbuf, rte_pktmbuf_pkt_len(mbuf));
                 goto out;
             }
@@ -85,7 +86,7 @@ void ss_frame_handle(struct rte_mbuf* mbuf, unsigned int port_id) {
         }
         case ETHER_TYPE_IPV6: {
             if (rx_buf.data.length < sizeof(eth_hdr_t) + sizeof(ip6_hdr_t)) {
-                RTE_LOG(ERR, SS, "received runt IPv6 frame of length %u:\n", rx_buf.data.length);
+                RTE_LOG(ERR, STACK, "received runt IPv6 frame of length %u:\n", rx_buf.data.length);
                 rte_pktmbuf_dump(stderr, mbuf, rte_pktmbuf_pkt_len(mbuf));
                 goto out;
             }
@@ -94,7 +95,7 @@ void ss_frame_handle(struct rte_mbuf* mbuf, unsigned int port_id) {
         }
         default: {
             if (rte_get_log_level() >= RTE_LOG_INFO) {
-                RTE_LOG(INFO, SS, "port %u received unsupported 0x%04hx frame:\n", port_id, ether_type);
+                RTE_LOG(INFO, STACK, "port %u received unsupported 0x%04hx frame:\n", port_id, ether_type);
                 rte_pktmbuf_dump(stderr, mbuf, rte_pktmbuf_pkt_len(mbuf));
             }
             break;
@@ -105,7 +106,7 @@ void ss_frame_handle(struct rte_mbuf* mbuf, unsigned int port_id) {
     
     rv = ss_extract_eth(&rx_buf);
     if (rv) {
-        RTE_LOG(WARNING, SS, "port %u ethernet RX hook failed\n", port_id);
+        RTE_LOG(WARNING, STACK, "port %u ethernet RX hook failed\n", port_id);
         rte_pktmbuf_dump(stderr, mbuf, rte_pktmbuf_pkt_len(mbuf));
     }
 
@@ -115,17 +116,17 @@ void ss_frame_handle(struct rte_mbuf* mbuf, unsigned int port_id) {
     }
 
     if (tx_buf.active && tx_buf.mbuf) {
-        RTE_LOG(INFO, SS, "sending tx_buf size %d\n", rte_pktmbuf_pkt_len(tx_buf.mbuf));
+        RTE_LOG(INFO, STACK, "sending tx_buf size %d\n", rte_pktmbuf_pkt_len(tx_buf.mbuf));
         rv = ss_send_packet(tx_buf.mbuf, tx_buf.data.port_id);
         if (rv) {
-            RTE_LOG(ERR, SS, "could not transmit tx_buf, rv: %d\n", rv);
+            RTE_LOG(ERR, STACK, "could not transmit tx_buf, rv: %d\n", rv);
             // XXX: what would we do here?
             rte_pktmbuf_free(tx_buf.mbuf);
             tx_buf.mbuf = NULL;
         }
     }
     else {
-        RTE_LOG(DEBUG, SS, "not sending tx_buf marked inactive\n");
+        RTE_LOG(DEBUG, STACK, "not sending tx_buf marked inactive\n");
         if (tx_buf.mbuf) {
             rte_pktmbuf_dump(stderr, tx_buf.mbuf, rte_pktmbuf_pkt_len(tx_buf.mbuf));
             rte_pktmbuf_free(tx_buf.mbuf);
@@ -139,23 +140,23 @@ int ss_frame_prepare_eth(ss_frame_t* tx_buf, int port_id, eth_addr_t* d_addr, ui
 
     tx_buf->mbuf = rte_pktmbuf_alloc(ss_pool);
     if (tx_buf->mbuf == NULL) {
-        RTE_LOG(ERR, SS, "could not allocate ethernet mbuf\n");
+        RTE_LOG(ERR, STACK, "could not allocate ethernet mbuf\n");
         goto error_out;
     }
 
     rte_pktmbuf_reset(tx_buf->mbuf);
     tx_buf->eth = (eth_hdr_t*) rte_pktmbuf_append(tx_buf->mbuf, sizeof(eth_hdr_t));
     if (tx_buf->eth == NULL) {
-        RTE_LOG(ERR, SS, "could not allocate ethernet mbuf\n");
+        RTE_LOG(ERR, STACK, "could not allocate ethernet mbuf\n");
         goto error_out;
     }
     ether_addr_copy(d_addr, &tx_buf->eth->d_addr);
     ether_addr_copy(&port_eth_addrs[port_id], &tx_buf->eth->s_addr);
     tx_buf->eth->ether_type = rte_bswap16(type);
-    RTE_LOG(INFO, SS, "eth src %02x:%02x:%02x:%02x:%02x:%02x\n",
+    RTE_LOG(INFO, STACK, "eth src %02x:%02x:%02x:%02x:%02x:%02x\n",
         tx_buf->eth->s_addr.addr_bytes[0], tx_buf->eth->s_addr.addr_bytes[1], tx_buf->eth->s_addr.addr_bytes[2],
         tx_buf->eth->s_addr.addr_bytes[3], tx_buf->eth->s_addr.addr_bytes[4], tx_buf->eth->s_addr.addr_bytes[5]);
-    RTE_LOG(INFO, SS, "eth dst %02x:%02x:%02x:%02x:%02x:%02x\n",
+    RTE_LOG(INFO, STACK, "eth dst %02x:%02x:%02x:%02x:%02x:%02x\n",
         tx_buf->eth->d_addr.addr_bytes[0], tx_buf->eth->d_addr.addr_bytes[1], tx_buf->eth->d_addr.addr_bytes[2],
         tx_buf->eth->d_addr.addr_bytes[3], tx_buf->eth->d_addr.addr_bytes[4], tx_buf->eth->d_addr.addr_bytes[5]);
     tx_buf->active = 1;
@@ -180,7 +181,7 @@ int ss_frame_handle_arp(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
     int rv = 0;
 
     rx_buf->arp = (arp_hdr_t*) ((uint8_t*) rte_pktmbuf_mtod(rx_buf->mbuf, uint8_t*) + sizeof(eth_hdr_t));
-    RTE_LOG(DEBUG, SS, "eth size %ld\n", (uint8_t*) rx_buf->arp - (uint8_t*) rx_buf->eth);
+    RTE_LOG(DEBUG, STACK, "eth size %ld\n", (uint8_t*) rx_buf->arp - (uint8_t*) rx_buf->eth);
     
     if (rte_get_log_level() >= RTE_LOG_INFO) {
         rte_memdump(stderr, "arp src", &rx_buf->arp->arp_sha, sizeof(rx_buf->arp->arp_sha));
@@ -193,19 +194,19 @@ int ss_frame_handle_arp(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
 
     int is_ip_daddr_ok = memcmp(&rx_buf->arp->arp_tpa, &ss_conf->ip4_address.ip4_addr, IPV4_ALEN) == 0;
     if (!is_ip_daddr_ok) {
-        RTE_LOG(INFO, SS, "arp request is not for this system, ignoring\n");
+        RTE_LOG(INFO, STACK, "arp request is not for this system, ignoring\n");
         goto error_out;
     }
 
     rv = ss_frame_prepare_eth(tx_buf, rx_buf->data.port_id, (eth_addr_t*) &rx_buf->eth->s_addr, ETHER_TYPE_ARP);
     if (rv) {
-        RTE_LOG(ERR, SS, "could not prepare ethernet mbuf\n");
+        RTE_LOG(ERR, STACK, "could not prepare ethernet mbuf\n");
         goto error_out;
     }
 
     tx_buf->arp = (arp_hdr_t*) rte_pktmbuf_append(tx_buf->mbuf, sizeof(arp_hdr_t));
     if (tx_buf->arp == NULL) {
-        RTE_LOG(ERR, SS, "could not allocate ethernet mbuf\n");
+        RTE_LOG(ERR, STACK, "could not allocate ethernet mbuf\n");
         goto error_out;
     }
     tx_buf->arp->arp_hrd = rte_bswap16(ARPHRD_ETHER);
@@ -224,7 +225,7 @@ int ss_frame_handle_arp(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
 
     error_out:
     if (tx_buf->mbuf) {
-        RTE_LOG(ERR, SS, "could not process arp frame\n");
+        RTE_LOG(ERR, STACK, "could not process arp frame\n");
         tx_buf->active = 0;
         rte_pktmbuf_free(tx_buf->mbuf);
         tx_buf->mbuf = NULL;
@@ -245,19 +246,19 @@ int ss_frame_handle_ndp(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
 
     int is_ndp_saddr_ok = memcmp(&rx_buf->ndp_rx->hdr.nd_ns_target, &ss_conf->ip6_address.ip6_addr, sizeof(rx_buf->ndp_rx->hdr.nd_ns_target)) == 0;
     if (!is_ndp_saddr_ok) {
-        RTE_LOG(INFO, SS, "ndp request is not for this system, ignoring\n");
+        RTE_LOG(INFO, STACK, "ndp request is not for this system, ignoring\n");
         goto error_out;
     }
 
     rv = ss_frame_prepare_eth(tx_buf, rx_buf->data.port_id, (eth_addr_t*) &rx_buf->eth->s_addr, ETHER_TYPE_IPV6);
     if (rv) {
-        RTE_LOG(ERR, SS, "could not prepare ethernet mbuf\n");
+        RTE_LOG(ERR, STACK, "could not prepare ethernet mbuf\n");
         goto error_out;
     }
 
     tx_buf->ip6 = (ip6_hdr_t*) rte_pktmbuf_append(tx_buf->mbuf, sizeof(ip6_hdr_t));
     if (tx_buf->ip6 == NULL) {
-        RTE_LOG(ERR, SS, "could not allocate mbuf ipv6 header\n");
+        RTE_LOG(ERR, STACK, "could not allocate mbuf ipv6 header\n");
         goto error_out;
     }
     tx_buf->ip6->ip6_flow = rte_bswap32(0x60000000);
@@ -270,7 +271,7 @@ int ss_frame_handle_ndp(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
     tx_buf->ndp_tx = (ndp_reply_t*) rte_pktmbuf_append(tx_buf->mbuf, sizeof(ndp_reply_t));
     tx_buf->icmp6  = (icmp6_hdr_t*) tx_buf->ndp_tx;
     if (tx_buf->ndp_tx == NULL) {
-        RTE_LOG(ERR, SS, "could not allocate mbuf ndp header\n");
+        RTE_LOG(ERR, STACK, "could not allocate mbuf ndp header\n");
         goto error_out;
     }
     tx_buf->ndp_tx->hdr.nd_na_type     = ND_NEIGHBOR_ADVERT;
@@ -293,7 +294,7 @@ int ss_frame_handle_ndp(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
 
     rv = ss_frame_prepare_icmp6(tx_buf, (uint8_t*) tx_buf->ndp_tx, sizeof(ndp_reply_t));
     if (rv) {
-        RTE_LOG(ERR, SS, "could not prepare ndp frame\n");
+        RTE_LOG(ERR, STACK, "could not prepare ndp frame\n");
         goto error_out;
     }
 
@@ -301,7 +302,7 @@ int ss_frame_handle_ndp(ss_frame_t* rx_buf, ss_frame_t* tx_buf) {
 
     error_out:
     if (tx_buf->mbuf) {
-        RTE_LOG(ERR, SS, "could not process ndp frame\n");
+        RTE_LOG(ERR, STACK, "could not process ndp frame\n");
         tx_buf->active = 0;
         rte_pktmbuf_free(tx_buf->mbuf);
         tx_buf->mbuf = NULL;
