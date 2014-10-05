29c37b5b4cab3e81d64e425db0bad52138935de6
Matthew Hall <mhall@mhcomputing.net>
2014-10-05 15:30:32 -0700

    extractor.h: remove old prototypes --> re_utils

--- src/extractor.h
+++ src/extractor.h
@@ -12,8 +12,6 @@ int ss_extract_eth(ss_frame_t* fbuf);
 int ss_extract_dns(ss_frame_t* fbuf);
 int ss_extract_dns_atype(ss_answer_t* result, dns_answer_t* aptr);
 int ss_extract_syslog(ss_frame_t* fbuf);
-int ss_extract_syslog_complete(ss_frame_t* fbuf, ss_re_entry_t* rptr);
-int ss_extract_syslog_substring(ss_frame_t* fbuf, ss_re_entry_t* rptr);
 
 /* END PROTOTYPES */
 
