4098b266edcfa61aaca3929ae948c697b3d1ccf4
Matthew Hall <mhall@mhcomputing.net>
2014-10-11 17:41:01 +0000

    sloccount.bash: automatically change to correct dir to run

--- scripts/sloccount.bash
+++ scripts/sloccount.bash
@@ -2,4 +2,8 @@
 
 set -e -x
 
+script_directory="$(dirname $(readlink -f $BASH_SOURCE))"
+source "${script_directory}/../sdn_sensor_rc"
+cd "${build_directory}"
+
 sloccount --follow --personcost 130000 --addlang makefile external/dpdk-helpers scripts src
