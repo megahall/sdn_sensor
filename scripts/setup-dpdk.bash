70f9b4957ce1887c009437a1570196834090e1c7
Matthew Hall <mhall@mhcomputing.net>
2014-10-11 17:38:02 +0000

    setup-dpdk.bash: fix some typos in the path env vars

--- scripts/setup-dpdk.bash
+++ scripts/setup-dpdk.bash
@@ -3,28 +3,33 @@
 set -e -x
 
 script_directory="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
-
+source "${script_directory}/../sdn_sensor_rc"
 cd "${script_directory}"
 
 thread_count=$(grep '^core id' /proc/cpuinfo | sort -u | wc -l)
 thread_count=1
 
-export RTE_SDK="${script_directory}"
-export RTE_SDK_BIN="${script_directory}/build"
-#export RTE_OUTPUT="${script_directory}/build"
+export RTE_SDK="${build_directory}/external/dpdk"
+export RTE_SDK_BIN="${RTE_SDK}/build"
+#export RTE_OUTPUT="${RTE_SDK}/build"
 export RTE_TARGET="x86_64-native-linuxapp-clang"
 export RTE_ARCH="x86_64"
 
 export EXTRA_CFLAGS="-g -O0 -fPIC -msse4"
 
-#make config T="${RTE_TARGET}"
-if [[ ! -f "build/.config" ]]; then
-    cp config.txt build/.config
+cd "${RTE_SDK}"
+
+if [[ ! -f ${RTE_SDK_BIN}/.config ]]; then
+    mkdir -p "${RTE_SDK_BIN}"
+    make config T="${RTE_TARGET}"
+    cp "${script_directory}/dpdk-config.txt" "${RTE_SDK_BIN}/.config"
 fi
 
-make clean
+#make clean
 make -j "${thread_count}"
-make -j "${thread_count}" -C examples RTE_SDK=$(pwd) RTE_TARGET=build RTE_SDK_BIN=$(pwd)/build
+make -j "${thread_count}" -C examples "RTE_SDK=${RTE_SDK}" "RTE_TARGET=build" "RTE_SDK_BIN=${RTE_SDK}/build"
 
 sudo cp build/build/lib/librte_eal/linuxapp/igb_uio/igb_uio.ko /lib/modules/$(uname -r)/kernel/drivers/uio/igb_uio.ko
 sudo depmod -a
+
+exit 0
