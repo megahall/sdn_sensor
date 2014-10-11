56648c5c2b13c6894e6ca5ff2412ddb6bb667fc7
Matthew Hall <mhall@mhcomputing.net>
2014-10-11 17:32:58 +0000

    scripts: add vagrant setup script, fix debian script, add rc files

--- scripts/setup-debian.bash
+++ scripts/setup-debian.bash
@@ -2,23 +2,56 @@
 
 set -e -x
 
-sudo apt-get -y install aptitude
+script_directory="$(dirname $(readlink -f $BASH_SOURCE))"
+source "${script_directory}/../sdn_sensor_rc"
+cd "${script_directory}"
 
-sudo aptitude -y purge command-not-found command-not-found-data python3-commandnotfound ufw
+cat > /etc/hosts <<'EOF'
+# The following lines are desirable for IPv6 capable hosts
+::1 ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
 
-sudo aptitude -y install linux-server linux-headers-server
+127.0.0.1 sdn-sensor
+EOF
 
-sudo aptitude -y install gawk strace ltrace telnet netcat-traditional tshark ssh rsync cproto cscope
+hostname sdn-sensor
 
-sudo aptitude -y install libjson-pp-perl libperl6-slurp-perl libdigest-sha-perl
+cat "${script_directory}/ackrc" > "/home/vagrant/.ackrc"
+cat "${script_directory}/inputrc" > "/etc/inputrc"
+cat "${script_directory}/sources.list" > "/etc/apt/sources.list"
 
-sudo aptitude -y install build-essential libc6-dbg clang llvm-gcc flex bison iwyu gdb-multiarch gdb-doc valgrind autoconf automake libtool git git-man git-email subversion manpages-dev manpages-posix-dev
+apt-get -y install aptitude
+aptitude update
+aptitude dist-upgrade
 
-sudo aptitude -y install uthash-dev libbsd-dev libpcre3-dev zlib1g-dev libglib2.0-dev gnulib libjson-c-dev libjson-c-doc liblog4c-dev liblog4c-doc libpcap-dev libfuse-dev libevtlog-dev libgeoip-dev geoip-bin libnet1-dev
+# remove unused SCM clients for performance reasons
+aptitude purge puppet puppet-common chef chef-zero
+
+aptitude -y purge command-not-found command-not-found-data python3-commandnotfound ufw
+
+aptitude -y install linux-server linux-headers-server
+
+aptitude -y install gawk strace ltrace telnet netcat-traditional tshark ssh rsync cproto cscope
+
+aptitude -y install libjson-pp-perl libperl6-slurp-perl libdigest-sha-perl libfile-next-perl
+
+aptitude -y install build-essential libc6-dbg clang llvm-gcc-4.7 flex bison iwyu gdb-multiarch gdb-doc valgrind autoconf automake libtool git git-man git-email subversion manpages-dev manpages-posix-dev
+
+aptitude -y install uthash-dev libbsd-dev libpcre3-dev zlib1g-dev libglib2.0-dev gnulib libjson-c-dev libjson-c-doc liblog4c-dev liblog4c-doc libpcap-dev libfuse-dev libevtlog-dev libgeoip-dev geoip-bin libnet1-dev
 
 # jemalloc
-sudo aptitude -y install libunwind-setjmp0 libunwind-setjmp0-dev libunwind8 libunwind8-dev docbook-xml docbook-xsl sgml-data xsltproc
+aptitude -y install libunwind-setjmp0 libunwind-setjmp0-dev libunwind8 libunwind8-dev docbook-xml docbook-xsl sgml-data xsltproc
 
+wget --timestamping http://snapshot.debian.org/archive/debian/20120609T102152Z/pool/main/a/ack-grep/ack-grep_1.96-2_all.deb
+dpkg -i ack-grep_1.96-2_all.deb
+dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
+echo "ack-grep hold" | dpkg --set-selections
 
 # python2.7
-# sudo aptitude -y install nginx nginx-extras
+# aptitude -y install nginx nginx-extras
+
+#aptitude clean
--- scripts/setup-vagrant.bash
+++ scripts/setup-vagrant.bash
@@ -2,10 +2,26 @@
 
 set -e -x
 
-script_directory=$(dirname $(readlink -f $BASH_SOURCE))
+script_directory="$(dirname $(readlink -f $BASH_SOURCE))"
+source "${script_directory}/../sdn_sensor_rc"
+cd "${build_directory}"
 
-echo "running Debian package setup"
-"${script_directory}/setup-debian.bash"
+cat "${script_directory}/ackrc" > "/home/vagrant/.ackrc"
+
+# Git settings required for the sdn_sensor repo to work properly
+# XXX: git stuff installed as vagrant user, but here we're root
+#git config --global push.default simple
+#git config --global fetch.recursesubmodules true
+#git config --global format.signoff true
+#git submodule init
+#git fetch
+#git submodule update --recursive
+
+#echo "running Debian package setup"
+#sudo "${script_directory}/setup-debian.bash"
+
+#echo "running Intel DPDK library setup"
+#"${script_directory}/setup-dpdk.bash"
 
 echo "running jemalloc library setup"
 "${script_directory}/setup-jemalloc.bash"
