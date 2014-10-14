3a79a755a4daf6f96015f641f3265ad707234ca0
Matthew Hall <mhall@mhcomputing.net>
2014-10-14 07:49:14 +0000

    setup-debian.bash: add additional conf files needed in Vagrant

--- scripts/setup-debian.bash
+++ scripts/setup-debian.bash
@@ -21,23 +21,54 @@ EOF
 hostname sdn-sensor
 
 cat "${script_directory}/ackrc" > "/home/vagrant/.ackrc"
+chown vagrant.vagrant "/home/vagrant/.ackrc"
+cat "${script_directory}/nanorc" > "/home/vagrant/.nanorc"
+chown vagrant.vagrant "/home/vagrant/.nanorc"
 cat "${script_directory}/inputrc" > "/etc/inputrc"
 cat "${script_directory}/sources.list" > "/etc/apt/sources.list"
+sed -i -e 's/^color ,green/#color ,green/' /usr/share/nano/*.nanorc
+
+sed -i \
+-e 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="hugepages=64"/' \
+-e 's/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="hugepages=64"/' \
+/etc/default/grub
+update-grub
+
+found_hugetlbfs=$(grep "/hugetlbfs" /etc/fstab || true)
+if [[ -z ${found_hugetlbfs} ]]; then
+    echo "nodev           /hugetlbfs      hugetlbfs defaults        0       0" >> /etc/fstab
+fi
+
+if [[ ! -e "/hugetlbfs" ]]; then
+    mkdir /hugetlbfs
+    mount /hugetlbfs
+fi
+
+sed -i \
+-e 's/^net\.ipv6\.conf\.all\.use_tempaddr.*/net.ipv6.conf.all.use_tempaddr = 0/g' \
+-e 's/^net\.ipv6\.conf\.default\.use_tempaddr.*/net.ipv6.conf.default.use_tempaddr = 0/g' \
+/etc/sysctl.d/10-ipv6-privacy.conf
+sysctl -p /etc/sysctl.d/10-ipv6-privacy.conf
+
+found_uio=$(egrep "^uio" /etc/modules || true)
+if [[ -z ${found_uio} ]]; then
+    echo "uio" >> /etc/modules
+    echo "igb_uio" >> /etc/modules
+fi
 
 apt-get -y install aptitude
 aptitude update
-aptitude dist-upgrade
-
-# remove unused SCM clients for performance reasons
-aptitude purge puppet puppet-common chef chef-zero
+aptitude -y dist-upgrade
 
 aptitude -y purge command-not-found command-not-found-data python3-commandnotfound ufw
+# remove unused SCM clients for performance reasons
+aptitude -y purge puppet puppet-common chef chef-zero
 
-aptitude -y install linux-server linux-headers-server
+aptitude -y install linux-generic linux-headers-generic
 
 aptitude -y install gawk strace ltrace telnet netcat-traditional tshark ssh rsync cproto cscope
 
-aptitude -y install libjson-pp-perl libperl6-slurp-perl libdigest-sha-perl libfile-next-perl
+aptitude -y install perl-doc libjson-pp-perl libperl6-slurp-perl libdigest-sha-perl libfile-next-perl
 
 aptitude -y install build-essential libc6-dbg clang llvm-gcc-4.7 flex bison iwyu gdb-multiarch gdb-doc valgrind autoconf automake libtool git git-man git-email subversion manpages-dev manpages-posix-dev
 
