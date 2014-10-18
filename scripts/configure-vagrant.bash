3c2c33b5663d7dda2b2b9d83fe99b022f16bc7c5
Matthew Hall <mhall@mhcomputing.net>
2014-10-18 23:41:34 +0000

    configure-vagrant.bash: add steps to enable Intel NIC

--- scripts/configure-vagrant.bash
+++ scripts/configure-vagrant.bash
@@ -2,11 +2,20 @@
 
 set -e -x
 
+if [[ $EUID -ne 0 ]]; then
+    echo "$0 must be run as root"
+    exit 1
+fi
+
 script_directory="$(dirname $(readlink -f $BASH_SOURCE))"
 source "${script_directory}/../sdn_sensor_rc"
 
+export RTE_SDK="${build_directory}/external/dpdk"
+export RTE_TOOLS="${RTE_SDK}/tools"
+export RTE_NIC_BIND="${RTE_TOOLS}/dpdk_nic_bind.py"
+
 export CORE_MASK=$("${script_directory}/find-dpdk-settings.pl" -c)
-export PCI_ID=$("${script_directory}/find-dpdk-settings.pl" -v | awk '/^virtio1 / { print $2; }')
+export PCI_ID=$("${script_directory}/find-dpdk-settings.pl" -p | awk '/^eth1 / { print $2; }')
 
 echo "CORE_MASK ${CORE_MASK}"
 echo "PCI_ID ${PCI_ID}"
@@ -17,5 +26,10 @@ conf_output="${script_directory}/../conf/sdn_sensor_vagrant.json"
 if [[ ! -e $conf_output ]]; then
     cat "${conf_input}" | envsubst > "${conf_output}"
 fi
+
+"${RTE_NIC_BIND}" --status | fgrep "${PCI_ID}"
+"${RTE_NIC_BIND}" -b none          "${PCI_ID}"
+"${RTE_NIC_BIND}" -b igb_uio       "${PCI_ID}"
+"${RTE_NIC_BIND}" --status | fgrep "${PCI_ID}"
 
 exit 0
