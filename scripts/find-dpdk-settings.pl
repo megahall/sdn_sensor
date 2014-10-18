b233de1dd180794e0c5807ce989148dfe19e2abf
Matthew Hall <mhall@mhcomputing.net>
2014-10-18 23:42:19 +0000

    find-dpdk-settings.pl: change from virtio-net to Intel NICs

--- scripts/find-dpdk-settings.pl
+++ scripts/find-dpdk-settings.pl
@@ -16,31 +16,32 @@ sub find_cpu_core_mask {
     printf("0x%08d\n", $core_count);
 }
 
-sub find_virtio_pci_ids {
+sub find_ethernet_pci_ids {
     # /sys/devices/pci0000:00/0000:00:03.0/virtio0
-    # /sys/devices/pci0000:00/0000:00:08.0/virtio1
+    # /sys/devices/pci0000:00/0000:00:03.0/net/eth0
     chdir("/sys/devices");
-    foreach my $virtio_path (<pci*/*/virtio*>) {
+    foreach my $ethernet_path (<pci*/*/net/eth*>) {
+        # pci0000:00/0000:00:08.0/net/eth0
         # pci0000:00/0000:00:03.0/virtio0
-        my ($pci_bus, $pci_id, $virtio_id) = split("/", $virtio_path, 3);
+        my ($pci_bus, $pci_id, $junk, $ethernet_id) = split("/", $ethernet_path, 4);
         # goal: virtio0 00:03.0
         $pci_id =~ s/^\d{4}://;
-        print "$virtio_id $pci_id\n";
+        print "$ethernet_id $pci_id\n";
     }
 }
 
 my $options = {};
 
-getopts("cv", $options) or die "invalid options provided: usage: $0 [-c] [-v]";
+getopts("cp", $options) or die "invalid options provided: usage: $0 [-c] [-p]";
 
 if ($options->{'c'}) {
     find_cpu_core_mask();
 }
-elsif ($options->{'v'}) {
-    find_virtio_pci_ids();
+elsif ($options->{'p'}) {
+    find_ethernet_pci_ids();
 }
 else {
-    die "no options provided: usage: $0 [-c] [-v]";
+    die "no options provided: usage: $0 [-c] [-p]";
 }
 
 exit(0);
