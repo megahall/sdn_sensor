1fad7d93d9b6d39f78700f69f9a18592f4d94fae
Matthew Hall <mhall@mhcomputing.net>
2014-10-23 03:48:33 +0000

    setup-perl.bash: use better wget output format

--- scripts/setup-perl.bash
+++ scripts/setup-perl.bash
@@ -26,7 +26,7 @@ libtest-fatal-perl libtest-sharedfork-perl libtest-tcp-perl
 cat "${script_directory}/ackrc" > "/home/vagrant/.ackrc"
 chown vagrant.vagrant "/home/vagrant/.ackrc"
 
-wget --timestamping http://snapshot.debian.org/archive/debian/20120609T102152Z/pool/main/a/ack-grep/ack-grep_1.96-2_all.deb
+wget --progress=dot:mega --timestamping http://snapshot.debian.org/archive/debian/20120609T102152Z/pool/main/a/ack-grep/ack-grep_1.96-2_all.deb
 sudo dpkg -i ack-grep_1.96-2_all.deb
 sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
 echo "ack-grep hold" | sudo dpkg --set-selections
