a45163c7a062e603ef474fcb17da22fa7dc16c41
Matthew Hall <mhall@mhcomputing.net>
2014-10-23 03:49:51 +0000

    setup-vagrant.bash: ssh_config, reorder git, nanomsg, add spcdns

--- scripts/setup-vagrant.bash
+++ scripts/setup-vagrant.bash
@@ -6,17 +6,10 @@ script_directory="$(dirname $(readlink -f $BASH_SOURCE))"
 source "${script_directory}/../sdn_sensor_rc"
 cd "${build_directory}"
 
-# Git settings required for the sdn_sensor repo to work properly
-# XXX: git stuff installed as vagrant user, but here we're root
-git config --global push.default simple
-git config --global fetch.recursesubmodules true
-git config --global format.signoff true
-git submodule init
-git fetch
-git submodule update --recursive
-
 cat "${script_directory}/ackrc" > "${HOME}/.ackrc"
 cat "${script_directory}/nanorc" > "${HOME}/.nanorc"
+mkdir -p "${HOME}/.ssh"
+cat "${script_directory}/ssh_config" > "${HOME}/.ssh/config"
 
 echo "running vagrant sudo setup"
 sudo "${script_directory}/setup-vagrant-sudo.bash"
@@ -24,19 +17,29 @@ sudo "${script_directory}/setup-vagrant-sudo.bash"
 echo "running Debian package setup"
 sudo "${script_directory}/setup-debian.bash"
 
-echo "running Perl package setup"
-sudo "${script_directory}/setup-perl.bash"
-
-echo "running Intel DPDK library setup"
-"${script_directory}/setup-dpdk.bash"
-
-echo "running jemalloc library setup"
-"${script_directory}/setup-jemalloc.bash"
+# Git settings required for the sdn_sensor repo to work properly
+# Git must be installed first before the steps will succeed
+git config --global push.default simple
+git config --global fetch.recursesubmodules true
+git config --global format.signoff true
+git submodule init
+git fetch
+git submodule update --recursive
 
 echo "running nanomsg library setup"
 "${script_directory}/setup-nanomsg.bash"
 
+echo "running Perl package setup"
+sudo "${script_directory}/setup-perl.bash"
+
+echo "running jemalloc library setup"
+"${script_directory}/setup-jemalloc.bash"
+
 echo "running re2 library setup"
 "${script_directory}/setup-re2.bash"
 
+echo "running spcdns library setup"
+"${script_directory}/setup-spcdns.bash"
+
+echo "vagrant phase 1 setup completed"
 exit 0
