3476e86372c8edb93eba5135c7fb00a17ae586af
Matthew Hall <mhall@mhcomputing.net>
2014-10-26 08:15:06 +0000

    netflow_format.c: add netflow metadata prepare

--- src/netflow_format.c
+++ src/netflow_format.c
@@ -29,6 +29,10 @@
 
 #include <bsd/string.h>
 
+#include <jemalloc/jemalloc.h>
+
+#include "ioc.h"
+#include "je_utils.h"
 #include "netflow_common.h"
 #include "netflow_format.h"
 #include "netflow_crc32.h"
@@ -113,6 +117,150 @@ u_int32_t netflow_swp_fake32(u_int32_t v) {
 
 u_int16_t netflow_swp_fake16(u_int16_t v) {
     return v;
+}
+
+uint8_t* ss_metadata_prepare_netflow(
+    const char* source, nn_queue_t* nn_queue,
+    struct store_flow_complete* flow,
+    ss_ioc_entry_t* ioc_entry) {
+    json_object* jobject    = NULL;
+    json_object* item    = NULL;
+    uint8_t*     rv      = NULL;
+    uint8_t*     jstring = NULL;
+
+    uint32_t fields;
+    uint64_t (*fmt_ntoh64)(uint64_t) = netflow_swp_ntoh64;
+    uint32_t (*fmt_ntoh32)(uint32_t) = netflow_swp_ntoh32;
+    uint16_t (*fmt_ntoh16)(uint16_t) = netflow_swp_ntoh16;
+    
+    uint32_t display_mask = STORE_DISPLAY_ALL;
+    fields = fmt_ntoh32(flow->hdr.fields) & display_mask;
+    int utc_flag = 0;
+
+    jobject = json_object_new_object();
+    if (jobject == NULL) {
+        RTE_LOG(ERR, EXTRACTOR, "could not allocate netflow json object\n");
+        goto error_out;
+    }
+
+    if (SHASFIELD(TAG)) {
+        item = json_object_new_int(fmt_ntoh32(flow->tag.tag));
+        json_object_object_add(jobject, "tag", item);
+    }
+    if (SHASFIELD(RECV_TIME)) {
+        item = json_object_new_string(iso_time(fmt_ntoh32(flow->recv_time.recv_sec), utc_flag));
+        json_object_object_add(jobject, "flow_sec", item);
+        item = json_object_new_int(fmt_ntoh32(flow->recv_time.recv_usec));
+        json_object_object_add(jobject, "flow_usec", item);
+    }
+    if (SHASFIELD(PROTO_FLAGS_TOS)) {
+        item = json_object_new_int(flow->pft.protocol);
+        json_object_object_add(jobject, "ip_protocol", item);
+        item = json_object_new_int(flow->pft.tcp_flags);
+        json_object_object_add(jobject, "tcp_flags", item);
+        item = json_object_new_int(flow->pft.tos);
+        json_object_object_add(jobject, "ip_tos", item);
+    }
+    if (SHASFIELD(AGENT_ADDR4) || SHASFIELD(AGENT_ADDR6)) {
+        item = json_object_new_string(addr_ntop_buf(&flow->agent_addr));
+        json_object_object_add(jobject, "agentip", item);
+    }
+    if (SHASFIELD(SRC_ADDR4) || SHASFIELD(SRC_ADDR6)) {
+        item = json_object_new_string(addr_ntop_buf(&flow->src_addr));
+        json_object_object_add(jobject, "sip", item);
+        item = json_object_new_int(fmt_ntoh16(flow->ports.src_port));
+        json_object_object_add(jobject, "sport", item);
+    }
+    if (SHASFIELD(DST_ADDR4) || SHASFIELD(DST_ADDR6)) {
+        item = json_object_new_string(addr_ntop_buf(&flow->dst_addr));
+        json_object_object_add(jobject, "dip", item);
+        item = json_object_new_int(fmt_ntoh16(flow->ports.dst_port));
+        json_object_object_add(jobject, "dport", item);
+    }
+    if (SHASFIELD(GATEWAY_ADDR4) || SHASFIELD(GATEWAY_ADDR6)) {
+        item = json_object_new_string(addr_ntop_buf(&flow->gateway_addr));
+        json_object_object_add(jobject, "gatewayip", item);
+    }
+    if (SHASFIELD(PACKETS)) {
+        item = json_object_new_int(fmt_ntoh64(flow->packets.flow_packets));
+        json_object_object_add(jobject, "packets", item);
+    }
+    if (SHASFIELD(OCTETS)) {
+        item = json_object_new_int(fmt_ntoh64(flow->octets.flow_octets));
+        json_object_object_add(jobject, "bytes", item);
+    }
+    if (SHASFIELD(IF_INDICES)) {
+        item = json_object_new_int(fmt_ntoh32(flow->ifndx.if_index_in));
+        json_object_object_add(jobject, "sifindex", item);
+        item = json_object_new_int(fmt_ntoh32(flow->ifndx.if_index_out));
+        json_object_object_add(jobject, "difindex", item);
+    }
+    if (SHASFIELD(AGENT_INFO)) {
+        item = json_object_new_string(interval_time(fmt_ntoh32(flow->ainfo.sys_uptime_ms) / 1000));
+        json_object_object_add(jobject, "sys_uptime_sec", item);
+        item = json_object_new_int(fmt_ntoh32(flow->ainfo.sys_uptime_ms) % 1000);
+        json_object_object_add(jobject, "sys_uptime_msec", item);
+        item = json_object_new_string(iso_time(fmt_ntoh32(flow->ainfo.time_sec), utc_flag));
+        json_object_object_add(jobject, "sys_time_sec", item);
+        item = json_object_new_int((u_long)fmt_ntoh32(flow->ainfo.time_nanosec));
+        json_object_object_add(jobject, "sys_time_nsec", item);
+        item = json_object_new_int(fmt_ntoh16(flow->ainfo.netflow_version));
+        json_object_object_add(jobject, "netflow_version", item);
+    }
+    if (SHASFIELD(FLOW_TIMES)) {
+        item = json_object_new_string(interval_time(fmt_ntoh32(flow->ftimes.flow_start) / 1000));
+        json_object_object_add(jobject, "flow_start_sec", item);
+        item = json_object_new_int(fmt_ntoh32(flow->ftimes.flow_start) % 1000);
+        json_object_object_add(jobject, "flow_start_msec", item);
+        item = json_object_new_string(interval_time(fmt_ntoh32(flow->ftimes.flow_finish) / 1000));
+        json_object_object_add(jobject, "flow_stop_sec", item);
+        item = json_object_new_int(fmt_ntoh32(flow->ftimes.flow_finish) % 1000);
+        json_object_object_add(jobject, "flow_stop_msec", item);
+    }
+    if (SHASFIELD(AS_INFO)) {
+        item = json_object_new_int(fmt_ntoh32(flow->asinf.src_as));
+        json_object_object_add(jobject, "src_as", item);
+        item = json_object_new_int(fmt_ntoh32(flow->asinf.src_mask));
+        json_object_object_add(jobject, "src_masklen", item);
+        item = json_object_new_int(fmt_ntoh32(flow->asinf.dst_as));
+        json_object_object_add(jobject, "dst_as", item);
+        item = json_object_new_int(fmt_ntoh32(flow->asinf.dst_mask));
+        json_object_object_add(jobject, "dst_masklen", item);
+    }
+    if (SHASFIELD(FLOW_ENGINE_INFO)) {
+        item = json_object_new_int(fmt_ntoh16(flow->finf.engine_type));
+        json_object_object_add(jobject, "engine_type", item);
+        item = json_object_new_int(fmt_ntoh16(flow->finf.engine_id));
+        json_object_object_add(jobject, "engine_id", item);
+        item = json_object_new_int((u_long)fmt_ntoh32(flow->finf.flow_sequence));
+        json_object_object_add(jobject, "seq_num", item);
+        item = json_object_new_int((u_long)fmt_ntoh32(flow->finf.source_id));
+        json_object_object_add(jobject, "source_id", item);
+    }
+    if (SHASFIELD(CRC32)) {
+        item = json_object_new_int(fmt_ntoh32(flow->crc32.crc32));
+        json_object_object_add(jobject, "crc32", item);
+    }
+
+    item = NULL;
+    rv = 0;
+    
+    // XXX: NOTE: String pointer is internal to JSON object.
+    jstring = (uint8_t*) json_object_to_json_string_ext(jobject, JSON_C_TO_STRING_SPACED);
+    rv = (uint8_t*) je_strdup((char*)jstring);
+    if (!rv) goto error_out;
+    
+    json_object_put(jobject); jobject = NULL;
+    
+    return rv;
+    
+    error_out:
+    fprintf(stderr, "could not serialize packet metadata\n");
+    if (rv) { je_free(rv); rv = NULL; }
+    if (jobject) { json_object_put(jobject); jobject = NULL; }
+    if (item) { json_object_put(item); item = NULL; }
+
+    return NULL;
 }
 
 void netflow_format_flow(struct store_flow_complete* flow, char* buf, size_t len,
