70d781062c3649710a8eb8988ccdceea8f47cce1
Matthew Hall <mhall@mhcomputing.net>
2014-12-22 00:57:18 +0000

    extractor.h: remove stale prototype

--- src/extractor.h
+++ src/extractor.h
@@ -12,7 +12,6 @@ int ss_extract_eth(ss_frame_t* fbuf);
 int ss_extract_dns(ss_frame_t* fbuf);
 int ss_extract_dns_atype(ss_answer_t* result, dns_answer_t* aptr);
 int ss_extract_syslog(ss_frame_t* fbuf);
-int ss_extract_netflow(ss_frame_t* fbuf);
 
 /* END PROTOTYPES */
 
