c9483f6ea4ff59a2a096015b035c5479815f8831
Matthew Hall <mhall@mhcomputing.net>
2014-12-22 00:43:17 +0000

    setup-nanomsg.bash: introduce OS X compatibility for this

--- scripts/setup-nanomsg.bash
+++ scripts/setup-nanomsg.bash
@@ -2,7 +2,23 @@
 
 set -e -x
 
-script_directory="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
+# start with g* for OS X
+# then switch to without for Linux if needed
+os=$(uname -s)
+if [[ $os == "Darwin" ]]; then
+    readlink_path=$(which greadlink)
+    mktemp_path=$(which gmktemp)
+    gsed_path=$(which gsed)
+    is_linux=0
+else
+    readlink_path=$(which readlink)
+    mktemp_path=$(which mktemp)
+    gsed_path=$(which sed)
+    is_linux=1
+fi
+
+echo $BASH_SOURCE
+script_directory=$(dirname $(${readlink_path} -f $BASH_SOURCE))
 source "${script_directory}/../sdn_sensor_rc"
 
 cd "${build_directory}/external/nanomsg"
@@ -25,4 +41,6 @@ fi
 make clean
 make
 sudo make install
-sudo ldconfig
+if [[ $is_linux -gt 0 ]]; then
+    sudo ldconfig
+fi
