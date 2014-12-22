b72dde87d3fb05a57c1e1d1fa97b7e3b143fee70
Matthew Hall <mhall@mhcomputing.net>
2014-12-22 10:23:38 +0000

    setup-lmdb.bash: don't forget to cd to liblmdb build dir

--- scripts/setup-lmdb.bash
+++ scripts/setup-lmdb.bash
@@ -5,6 +5,8 @@ set -e -x
 script_directory="$(dirname $(readlink -f $BASH_SOURCE))"
 source "${script_directory}/../sdn_sensor_rc"
 
+cd "${build_directory}/external/lmdb/libraries/liblmdb"
+
 make clean
 make
 make test
