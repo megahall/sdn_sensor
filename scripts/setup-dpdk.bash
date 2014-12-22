66c443f2bd438c20c76c071ee9b59c429ddba8ee
Matthew Hall <mhall@mhcomputing.net>
2014-12-22 00:42:49 +0000

    setup-dpdk.bash: improve logic of script_directory

--- scripts/setup-dpdk.bash
+++ scripts/setup-dpdk.bash
@@ -2,7 +2,7 @@
 
 set -e -x
 
-script_directory="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
+script_directory="$(dirname $(readlink -f ${BASH_SOURCE}))"
 source "${script_directory}/../sdn_sensor_rc"
 cd "${script_directory}"
 
