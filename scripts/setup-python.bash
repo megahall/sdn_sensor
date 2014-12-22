a1d0330fe4e316ea78486a8ef05eae2255b99f65
Matthew Hall <mhall@mhcomputing.net>
2014-12-22 10:45:47 +0000

    setup-python.bash: install basic python packags for load_generator

---
+++ scripts/setup-python.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e -x
+
+script_directory="$(dirname $(readlink -f $BASH_SOURCE))"
+source "${script_directory}/../sdn_sensor_rc"
+cd "${script_directory}"
+
+sudo aptitude install \
+python-setuptools \
+python-pip \
+python-virtualenv \
+ipython \
+python-ipdb \
+python-netaddr
