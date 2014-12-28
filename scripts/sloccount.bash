6d091d2312531056daddce3ca4ecf36467f19a4d
Matthew Hall <mhall@mhcomputing.net>
2014-12-28 00:37:37 -0800

    scripts: mark all bash scripts w/ mode 755

--- scripts/sloccount.bash
+++ scripts/sloccount.bash
@@ -6,4 +6,4 @@ script_directory="$(dirname $(readlink -f $BASH_SOURCE))"
 source "${script_directory}/../sdn_sensor_rc"
 cd "${build_directory}"
 
-sloccount --follow --personcost 130000 --addlang makefile external/dpdk-helpers scripts src
+sloccount --follow --personcost 130000 --addlang makefile external/dpdk-helpers scripts src analytics
