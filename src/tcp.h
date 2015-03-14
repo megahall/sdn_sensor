3736f344b78b06bd4de88231f7fb8af0143278d3
Matthew Hall <mhall@mhcomputing.net>
2015-03-14 21:53:47 +0000

    tcp.h: prototypes

--- src/tcp.h
+++ src/tcp.h
@@ -6,11 +6,12 @@
 /* BEGIN PROTOTYPES */
 
 int ss_tcp_init(void);
+int ss_tcp_timer_callback(void);
 int ss_frame_handle_tcp(ss_frame_t* rx_buf, ss_frame_t* tx_buf);
 int ss_tcp_extract_syslog(ss_tcp_socket_t* socket, ss_frame_t* rx_buf);
 int ss_tcp_socket_init(ss_flow_key_t* key, ss_tcp_socket_t* socket);
 ss_tcp_socket_t* ss_tcp_socket_create(ss_flow_key_t* key, ss_frame_t* rx_buf);
-int ss_tcp_socket_delete(ss_flow_key_t* key);
+int ss_tcp_socket_delete(ss_flow_key_t* key, int is_locked);
 ss_tcp_socket_t* ss_tcp_socket_lookup(ss_flow_key_t* key);
 int ss_tcp_prepare_rx(ss_frame_t* rx_buf, ss_tcp_socket_t* socket);
 int ss_tcp_prepare_tx(ss_frame_t* tx_buf, ss_tcp_socket_t* socket, ss_tcp_state_t state);
