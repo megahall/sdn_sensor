fa964e4a26e9932b10eec883be4f7199c97fdae1
Matthew Hall <mhall@mhcomputing.net>
2016-01-22 21:10:29 -0800

    setup-dpdk.bash: use latest build procedures from mailing list

--- scripts/setup-dpdk.bash
+++ scripts/setup-dpdk.bash
@@ -7,30 +7,31 @@ source "${script_directory}/../sdn_sensor_rc"
 cd "${script_directory}"
 
 thread_count=$(grep '^core id' /proc/cpuinfo | sort -u | wc -l)
-thread_count=1
+#thread_count=1
 
 export RTE_SDK="${build_directory}/external/dpdk"
-export RTE_SDK_BIN="${RTE_SDK}/build"
-#export RTE_OUTPUT="${RTE_SDK}/build"
-export RTE_TARGET="x86_64-native-linuxapp-clang"
+export RTE_OUTPUT="${RTE_SDK}/build"
+export RTE_TARGET="x86_64-native-linuxapp-gcc"
 export RTE_ARCH="x86_64"
-export RTE_INCLUDE="${RTE_SDK_BIN}/include"
+export RTE_INCLUDE="${RTE_OUTPUT}/include"
 
-export EXTRA_CFLAGS="-g -O2 -fPIC -msse4.2"
+export EXTRA_CFLAGS="${CFLAGS}"
 
 cd "${RTE_SDK}"
 
-if [[ ! -f ${RTE_SDK_BIN}/.config ]]; then
-    mkdir -p "${RTE_SDK_BIN}"
+if [[ ! -f ${RTE_OUTPUT}/.config ]]; then
+    mkdir -p "${RTE_OUTPUT}"
     make config T="${RTE_TARGET}"
-    cp "${script_directory}/dpdk-config.txt" "${RTE_SDK_BIN}/.config"
+    #cp "${script_directory}/dpdk-config.txt" "${RTE_OUTPUT}/.config"
 fi
 
-#make clean || true
+make clean || true
 
-make -j "${thread_count}" "RTE_OUTPUT=${RTE_SDK_BIN}"
-make -j "${thread_count}" -C app/test "RTE_SDK=${RTE_SDK}" "RTE_TARGET=build" "RTE_SDK_BIN=${RTE_SDK}/build"
-make -j "${thread_count}" -C examples "RTE_SDK=${RTE_SDK}" "RTE_TARGET=build" "RTE_SDK_BIN=${RTE_SDK}/build"
+export MAKEFLAGS="-j${thread_count}"
+make "RTE_OUTPUT=${RTE_OUTPUT}"
+make install "T=${RTE_TARGET}" "O=build" "DESTDIR=build"
+make -C app/test "T=${RTE_TARGET}" "RTE_SDK=${RTE_SDK}" "RTE_TARGET=build"
+make -C examples "T=${RTE_TARGET}" "RTE_SDK=${RTE_SDK}" "RTE_TARGET=build"
 
 sudo mkdir -p /lib/modules/$(uname -r)/kernel/drivers/uio
 sudo cp build/build/lib/librte_eal/linuxapp/igb_uio/igb_uio.ko /lib/modules/$(uname -r)/kernel/drivers/uio/igb_uio.ko
